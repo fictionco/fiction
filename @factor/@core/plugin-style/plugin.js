1b80bf461d138aa318c8938667b8cf54e50865f3
Andrew Powers <andrew@fiction.com>
2019-04-13 15:05:16 -0600

    error fix

--- @factor/@core/plugin-style/plugin.js
+++ @factor/@core/plugin-style/plugin.js
@@ -4,7 +4,11 @@ export default Factor => {
       const { resolve } = require("path")
       Factor.$filters.add("prepended-style-var-files", _ => {
         _.push(resolve(__dirname, "style-vars*"))
-        _.push(resolve(Factor.$paths.get("theme"), "**/style-vars*"))
+
+        if (Factor.$paths.get("theme")) {
+          _.push(resolve(Factor.$paths.get("theme"), "**/style-vars*"))
+        }
+
         _.push(resolve(Factor.$paths.get("app"), "**/style-vars*"))
         return _
       })
