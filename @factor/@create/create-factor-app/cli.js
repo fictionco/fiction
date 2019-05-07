c1a2768deb49b7c2ccc5814d233ab09c21b2d351
Andrew Powers <andrew@fiction.com>
2019-05-07 13:17:18 -0400

    dep

--- @factor/@create/create-factor-app/cli.js
+++ @factor/@create/create-factor-app/cli.js
@@ -3,10 +3,12 @@ const path = require("path")
 const sao = require("sao")
 const consola = require("consola")
 const generator = path.resolve(__dirname, "./")
+const { name, version } = require("./package")
 // In a custom directory or current directory
 const outDir = path.resolve(process.argv[2] || ".")
 
-consola.success(`[Great work! Generating Factor project in "${outDir}"]`)
+consola.info(`Welcome to ${name}@${version}`)
+consola.success(`Great work! Generating Factor project in: ${outDir}`)
 
 // See https://sao.js.org/#/advanced/standalone-cli
 sao({ generator, outDir, logLevel: 2 })
