6ec10d93996450a5c681e3f93962089e133e0172
finestpixels <hello@finestpixels.com>
2019-05-10 14:01:19 -0400

    create factor app updated

---
+++ @factor/@create/create-factor-app/template/factor/src/_content.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="content-layout">
+    <div class="mast">
+      <fiction-pin class="icon" />
+      <h1>{{ siteName }}</h1>
+      <h4>{{ description }}</h4>
+      <div class="action">
+        <app-btn>Visit Docs</app-btn>
+      </div>
+    </div>
+  </div>
+</template>
+<style src="./css/style.less" lang="less"></style>
+<script>
+import { name, description } from "../package"
+export default {
+  components: {
+    "fiction-pin": () => import("./el/pin"),
+    "app-btn": () => import("./el/btn")
+  },
+  computed: {
+    siteName() {
+      return this.$utils.toLabel(name)
+    },
+    description() {
+      return description
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.content-layout {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  .mast {
+    text-align: center;
+    .icon {
+      margin-bottom: 1em;
+    }
+    h1,
+    h4 {
+      letter-spacing: -0.02em;
+    }
+    h1 {
+      font-size: 3em;
+    }
+
+    h4 {
+      font-size: 1.5em;
+      opacity: 0.4;
+    }
+    .action {
+      margin-top: 3em;
+    }
+  }
+}
+</style>
---
+++ @factor/@create/create-factor-app/template/factor/src/site-head.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="site-head">
+    <div class="site-head-pad">
+      <div class="brand">
+        <factor-link path="/">{{ siteName }}</factor-link>
+      </div>
+      <div class="nav">
+        <slot />
+        <plugin-signin-profile-menu v-if="$uid" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { name, description } from "../package"
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {
+    siteName() {
+      return this.$utils.toLabel(name)
+    }
+  }
+}
+</script>
+<style lang="less">
+.site-head {
+  padding: 0 1.5em;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.25), 0 1px 15px rgba(0, 0, 0, 0.03);
+  background: #fff;
+  position: relative;
+  z-index: 10;
+}
+
+.site-head-pad {
+  height: 45px;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+
+  .brand {
+    display: flex;
+    flex-grow: 1;
+    align-items: center;
+    width: 100px;
+  }
+  .nav {
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+    @media (max-width: 767px) {
+      flex-grow: 2;
+      justify-content: flex-end;
+    }
+    > a {
+      letter-spacing: -0.03em;
+      font-size: 0.9em;
+      color: var(--color-text);
+      margin: 0 1em;
+      @media (max-width: 767px) {
+        margin: 0 0.5em;
+      }
+      &:hover,
+      &.active-path {
+        color: var(--color-primary);
+      }
+      &:active {
+        color: var(--color-secondary);
+      }
+    }
+    .profile-menu {
+      margin-left: 1em;
+      @media (max-width: 767px) {
+        display: none;
+      }
+    }
+  }
+}
+</style>
