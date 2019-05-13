765edd200af17ccf607b2a10dcba23f3e09ca4b7
Andrew Powers <andrew@fiction.com>
2019-05-13 16:52:11 -0400

    remove code

--- @factor/@core/core-stack/plugin.js
+++ @factor/@core/core-stack/plugin.js
@@ -57,36 +57,6 @@ module.exports = Factor => {
       })
     }
 
-    // verifyServices({ log = false } = {}) {
-    //   let needed = 0
-    //   let total = 0
-    //   this.serviceRequests.forEach(_ => {
-    //     const { id, description, args, result, title } = _
-
-    //     if (Factor.$filters.count(id) == 0) {
-    //       const lines = []
-
-    //       if (description) lines.push({ title: "Description", value: description })
-    //       if (args) lines.push({ title: "Arguments", value: args })
-    //       if (result) lines.push({ title: "Returns", value: result })
-
-    //       const message = {
-    //         title: `Service Handler Needed: ${title} (${id})`,
-    //         lines
-    //       }
-
-    //       if (log) {
-    //         Factor.$log.formatted(message)
-    //       }
-
-    //       needed++
-    //     }
-    //     total++
-    //   })
-
-    //   return { needed, total, setup: total - needed }
-    // }
-
     registerProvider(args) {
       this.providers.push(args)
     }
@@ -94,92 +64,5 @@ module.exports = Factor => {
     getProviders() {
       return this.providers
     }
-
-    // verifyProviders({ log = false } = {}) {
-    //   const config = Factor.$config.settings()
-    //   let needed = 0
-    //   let total = 0
-    //   let configure = []
-
-    //   this.providers.forEach(_ => {
-    //     let { provider, publicKeys, privateKeys, description, title, link, multiEnv = false } = _
-
-    //     const normalizeKeys = (keys = []) =>
-    //       keys.map(_ => {
-    //         if (typeof _ == "string") {
-    //           return { key: _, input: "string" }
-    //         } else {
-    //           return _
-    //         }
-    //       })
-
-    //     const settings = config[provider]
-    //     const keys = [
-    //       { scope: "public", keys: normalizeKeys(publicKeys) },
-    //       { scope: "private", keys: normalizeKeys(privateKeys) }
-    //     ]
-    //     const keysNeeded = []
-    //     let verified = true
-
-    //     keys.forEach(({ scope, keys }) => {
-    //       if (keys && keys.length > 0) {
-    //         keys.forEach(({ key, input }) => {
-    //           if (!settings || !settings[key]) {
-    //             verified = false
-    //             keysNeeded.push({ scope, key, input })
-    //           }
-    //         })
-    //       }
-    //     })
-
-    //     if (!verified) {
-    //       needed++
-    //       configure.push({ ..._, keysNeeded })
-    //     }
-
-    //     total++
-
-    //     // const groupTitle = `Configure Provider: ${title || Factor.$utils.toLabel(provider)}`
-    //     // const lines = []
-    //     // lines.push({ title: "Description", value: description })
-    //     // lines.push({ title: "Link", value: link })
-
-    //     // keys.forEach(({ scope, keys }) => {
-    //     //   if (keys && keys.length > 0) {
-    //     //     const keysNeeded = []
-    //     //     keys.forEach(key => {
-    //     //       if (!settings || !settings[key]) {
-    //     //         keysNeeded.push(key)
-    //     //       }
-    //     //     })
-
-    //     //     // if (keysNeeded.length > 0) {
-    //     //     //   verified = false
-    //     //     //   const file = scope == "public" ? "factor-config.json" : "factor-secrets.json"
-    //     //     //   lines.push({ title: `${Factor.$utils.toLabel(scope)} Config`, value: "" })
-    //     //     //   lines.push({ indent: true, title: "Keys", value: keysNeeded.map(_ => `[${_}]`).join(" ") })
-    //     //     //   lines.push({ indent: true, title: "To Fix", value: `Add under [env]:{${provider}} in [${file}]` })
-    //     //     // }
-    //     //   }
-    //     // })
-
-    //     // configure.push({ provider, privateKeys, publicKeys })
-
-    //     // if (!verified) {
-    //     //   if (log) {
-    //     //     const message = {
-    //     //       title: groupTitle,
-    //     //       lines
-    //     //     }
-    //     //     Factor.$log.formatted(message)
-    //     //   }
-
-    //     //   needed++
-    //     // }
-    //     // total++
-    //   })
-
-    //   return { needed, total, setup: total - needed, configure }
-    // }
   })()
 }
