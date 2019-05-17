6c0311967142dff9556e1ac8a52947f107a131af
Andrew Powers <andrew@fiction.com>
2019-05-17 15:20:53 -0400

    blog tweak

--- @factor/@core/plugin-blog/wrap.vue
+++ @factor/@core/plugin-blog/wrap.vue
@@ -9,15 +9,11 @@ export default {}
 </script>
 
 <style lang="less">
-:root {
-  --panel-shadow: 0 0 1px rgba(58, 55, 148, 0.25),
-    0 6px 14px 0 rgba(24, 32, 41, 0.06), 0 12px 34px 0 rgba(24, 32, 41, 0.04);
-}
 .blog-wrap {
   .grid-entry,
   .grid-aside,
   .related-entry {
-    box-shadow: var(--panel-shadow);
+    box-shadow: var(--input-shadow);
     background: #fff;
     border-radius: 5px;
   }
