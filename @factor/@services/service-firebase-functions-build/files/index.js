c402d5d932115ffd05962452d4336a6e5929440f
Andrew Powers <andrew@fiction.com>
2019-07-14 10:02:08 -0600

    move folders

--- @factor/@services/service-firebase-functions-build/files/index.js
+++
@@ -1,7 +0,0 @@
-const Factor = require("vue")
-Factor.FACTOR_CONFIG = { baseDir: __dirname }
-Factor.FACTOR_TARGET = "cloud"
-
-module.exports = require("@factor/service-firebase-functions-controller")
-  .default(Factor)
-  .endpoints()
