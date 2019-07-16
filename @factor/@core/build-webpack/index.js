7fa50941844906d60dee9c165635e38a7c8305d3
Andrew Powers <andrew@fiction.com>
2019-07-16 00:52:00 -0600

    HMR Fix

--- @factor/@core/build-webpack/index.js
+++ @factor/@core/build-webpack/index.js
@@ -217,6 +217,7 @@ module.exports.default = Factor => {
         output: {
           publicPath: Factor.$paths.get("dist")
         },
+        module: {},
         plugins: [
           // new HardSourceWebpackPlugin({
           //   info: {
