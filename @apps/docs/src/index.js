c5ff57de4995f2d2843306dbb4a2780e7437e0b2
Raymond Lopez Aleman <hello@finestpixels.com>
2019-07-22 13:51:53 -0600

    compare page (#16)

--- @apps/docs/src/index.js
+++ @apps/docs/src/index.js
@@ -10,7 +10,11 @@ module.exports.default = Factor => {
           {
             path: "/themes",
             component: () => import("./page-themes")
-          }
+          },
+          {
+            path: "/compare",
+            component: () => import("./page-compare")
+          },
         ]
 
         return _.concat(routes)
