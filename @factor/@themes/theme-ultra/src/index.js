5303cadacf629fcbf27a7264d8417cc952c44ce7
Edgar Cheverier <edgarcheverier@gmail.com>
2019-07-22 22:53:56 +0200

    Theme Ultra (#18)
    Starts setup for ultra theme, adds the home, about, services and
    portfolio pages, also adds sidebar for managing pages location.
    News and Contact pages are still missing, after that
    it's remaining to improve styles and clean the code.

---
+++ @factor/@themes/theme-ultra/src/index.js
@@ -0,0 +1,29 @@
+module.exports.default = Factor => {
+  return new(class {
+    constructor() {
+      this.addPaths()
+      this.addSettings()
+    }
+
+    addSettings() {
+      Factor.$filters.add("settings", _ => {
+        return [..._, require("./settings")(Factor)]
+      })
+    }
+
+    async addPaths() {
+
+      Factor.$filters.add("content-routes", _ => {
+        const routes = [{
+          path: "/",
+          component: () => import("./page-home"),
+          meta: {
+            background: "#fafbff"
+          }
+        }]
+
+        return _.concat(routes)
+      })
+    }
+  })()
+}
