95a0b3d6bd10e89eec59e53d4ac16bd10ade7376
Edgar Cheverier <edgarcheverier@gmail.com>
2019-07-27 18:23:05 +0200

    Theme Ultra, News and Contact pages. (#24)
    Adds the missing pages News and Contact to the theme,
    fixes styling issues such as switching classe names from
    PascalCase to kebab-case, also adds a basic readme.

--- @factor/@themes/theme-ultra/src/page-home.vue
+++ @factor/@themes/theme-ultra/src/page-home.vue
@@ -1,38 +1,37 @@
 <template>
-  <div id="homeContainerID" class="homeContainer">
-    <div class="homeQuotesContainer">
-      <h5 class="homeQuotes">{{ $setting.get('home.quotes') }}</h5>
+  <div id="homeContainerID" class="home-container">
+    <div class="home-quotes-container">
+      <h5 class="home-quotes">{{ $setting.get('home.quotes') }}</h5>
     </div>
-    <div class="homeMainQuotesContainer">
-      <h1 class="homeMainQuotes">{{ $setting.get('home.mainQuotes') }}</h1>
+    <div class="home-main-quotes-container">
+      <h1 class="home-main-quotes">{{ $setting.get('home.mainQuotes') }}</h1>
     </div>
-    <div class="homeButtonsContainer">
-      <div v-for="(ele, i) in $setting.get('home.homeButtons')" :key="i" class="homeButtons">
-        <button class="homeButton" :class="ele.class" @click="homePaths(ele.path)">{{ ele.text }}</button>
+    <div class="home-buttons-container">
+      <div v-for="(ele, i) in $setting.get('home.homeButtons')" :key="i" class="home-buttons">
+        <button class="home-button" :class="ele.class" @click="homePaths(ele.path)">{{ ele.text }}</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-/* eslint-disable */
 export default {
   components: {},
   data() {
     return {
       loading: true
-    };
+    }
   },
   methods: {
     homePaths(path) {
-      let ele = document.querySelector(path);
-      ele.scrollIntoView();
+      let ele = document.querySelector(path)
+      ele.scrollIntoView()
     }
   }
-};
+}
 </script>
 <style scoped>
-.homeContainer {
+.home-container {
   background: linear-gradient(294.11deg, #732b29 0%, #111010 100%);
   font-family: Work Sans;
   display: grid;
@@ -40,27 +39,27 @@ export default {
   grid-template-rows: 60px 160px 60px;
   height: 100%;
 }
-.homeQuotesContainer {
+.home-quotes-container {
   margin: 1.2vw 0 0 1.2vw;
 }
-.homeQuotes {
+.home-quotes {
   color: #9e9e9e;
   font-size: 1.4vw;
 }
-.homeMainQuotesContainer {
+.home-main-quotes-container {
   margin: 0.6vw 0 0 1.2vw;
   width: 60%;
 }
-.homeMainQuotes {
+.home-main-quotes {
   font-size: 2.7vw;
   color: #f7f7f7;
 }
-.homeButtonsContainer {
+.home-buttons-container {
   margin: 7vw 0 0 1.2px;
   display: flex;
   justify-content: center;
 }
-.homeButton {
+.home-button {
   width: 20vw;
   height: 8vh;
   outline: none;
--- @factor/@themes/theme-ultra/src/page-portfolio.vue
+++ @factor/@themes/theme-ultra/src/page-portfolio.vue
@@ -1,27 +1,39 @@
 <template>
-  <div id="portfolioContainerID" class="portfolioContainer">
-    <div class="portfolioHeaderContainer">
-      <h4 class="portfolioQuote">{{ $setting.get('portfolio.quote')}}</h4>
-      <h1 class="portfolioTitle">{{ $setting.get('portfolio.title')}}</h1>
+  <div id="portfolioContainerID" class="portfolio-container">
+    <div class="portfolio-header-container">
+      <h4 class="portfolio-quote">{{ $setting.get('portfolio.quote') }}</h4>
+      <h1 class="portfolio-title">{{ $setting.get('portfolio.title') }}</h1>
     </div>
-    <div class="portfolioGalleryContainer">
+    <div class="portfolio-gallery-container">
       <div
-        class="portfolioPictureContainer"
         v-for="(ele, i) in $setting.get('portfolio.pictures')"
         :key="i"
+        class="portfolio-picture-container"
       >
-        <img :class="ele.pictureClass" :src="ele.picture" :alt="ele.alt" />
+        <img :class="ele.pictureClass" :src="ele.picture" :alt="ele.alt" >
+      </div>
+    </div>
+    <div class="portfolio-previous-content-container">
+      <h1 class="portfolio-previous-title">{{ $setting.get('portfolio.previousTitle') }}</h1>
+      <div class="portfolio-clients-container">
+        <div
+          v-for="(ele, i) in $setting.get('portfolio.clients')"
+          :key="i"
+          class="portfolio-clients"
+        >
+          <img class="portfolio-clients-picture" :src="ele.picture" :alt="ele.alt" >
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style>
-.portfolioContainer {
+.portfolio-container {
   background: #111010;
   font-family: Work Sans;
   display: grid;
@@ -29,20 +41,20 @@ export default {};
   grid-template-rows: 60px 160px 60px;
   height: 100%;
 }
-.portfolioHeaderContainer {
+.portfolio-header-container {
   text-align: center;
   margin-top: 2vh;
 }
-.portfolioQuote {
+.portfolio-quote {
   color: #9e9e9e;
   font-size: 1.4vw;
 }
-.portfolioTitle {
+.portfolio-title {
   color: #f7f7f7;
   font-size: 2.2vw;
   font-weight: bold;
 }
-.portfolioGalleryContainer {
+.portfolio-gallery-container {
   margin: 15vh 0 0 26vw;
   width: 20vw;
   display: grid;
@@ -52,11 +64,38 @@ export default {};
     "div div";
   justify-items: center;
 }
-.portfolioPictureContainer {
+.portfolio-picture-container {
   width: 15vw;
   height: 32vh;
 }
 .pictureClasslong {
+  width: 100%;
+  height: 100%;
+}
+.portfolio-previous-content-container {
+  margin: 96vh 0 0 3.5vw;
+  text-align: center;
+}
+.portfolio-previous-title {
+  color: #f7f7f7;
+  font-size: 2.2vw;
+  font-weight: bold;
+}
+.portfolio-clients-container {
+  margin: 1vh 0 0 22vw;
+  width: 20vw;
+  display: grid;
+  grid-template-areas:
+    "div div div"
+    "div div div";
+  justify-items: center;
+}
+.portfolio-clients {
+  width: 10vw;
+  height: 9vh;
+  margin: 0.3vw;
+}
+.portfolio-clients-picture {
   width: 100%;
   height: 100%;
 }
