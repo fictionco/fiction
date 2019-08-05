663ee263aa0f2a62deac25d3e1d66133f77019d7
Andrew Powers <arpowers@gmail.com>
2019-08-05 13:53:53 -0600

    example customization

--- @apps/fiction/.factor/loader-settings.js
+++ @apps/fiction/.factor/loader-settings.js
@@ -1,4 +1,5 @@
 /******** GENERATED FILE ********/
 const files = {}
 files["blog"] = require("@factor/plugin-blog/theme-settings.js").default
+files["cwd"] = require("../src/theme-settings.js").default
 module.exports = files
--- @factor/@plugins/plugin-blog/theme-settings.js
+++ @factor/@plugins/plugin-blog/theme-settings.js
@@ -28,20 +28,20 @@ module.exports.default = Factor => {
         meta: ["authorDate", "tags"]
       },
       components: {
-        blogContent: () => import("#/blog-content.vue"),
-        blogIndex: () => import("#/blog-index.vue"),
-        blogSingle: () => import("#/blog-single.vue"),
-        pagination: () => import("#/widget-pagination.vue"),
-        returnLink: () => import("#/widget-return-link.vue"),
-        authorDate: () => import("#/widget-author-date.vue"),
-        authorBio: () => import("#/widget-author-bio.vue"),
-        entry: () => import("#/widget-entry.vue"),
-        excerpt: () => import("#/widget-excerpt.vue"),
-        featuredImage: () => import("#/widget-featured-image.vue"),
-        headers: () => import("#/widget-headers.vue"),
-        meta: () => import("#/widget-meta.vue"),
-        social: () => import("#/widget-social.vue"),
-        tags: () => import("#/widget-tags.vue")
+        blogContent: () => import("./blog-content.vue"),
+        blogIndex: () => import("./blog-index.vue"),
+        blogSingle: () => import("./blog-single.vue"),
+        pagination: () => import("./widget-pagination.vue"),
+        returnLink: () => import("./widget-return-link.vue"),
+        authorDate: () => import("./widget-author-date.vue"),
+        authorBio: () => import("./widget-author-bio.vue"),
+        entry: () => import("./widget-entry.vue"),
+        excerpt: () => import("./widget-excerpt.vue"),
+        featuredImage: () => import("./widget-featured-image.vue"),
+        headers: () => import("./widget-headers.vue"),
+        meta: () => import("./widget-meta.vue"),
+        social: () => import("./widget-social.vue"),
+        tags: () => import("./widget-tags.vue")
       }
     }
   }
