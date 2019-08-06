66a1f43eab2fc8b287ee623162235e195fc7a99f
Andrew Powers <arpowers@gmail.com>
2019-08-06 14:33:48 -0600

    add email to contact form

--- @apps/fiction/.factor/loader-settings.js
+++ @apps/fiction/.factor/loader-settings.js
@@ -1,6 +1,5 @@
 /******** GENERATED FILE ********/
 const files = {}
 files["blog"] = require("@factor/plugin-blog/factor-settings.js").default
-files["contactForm"] = require("@factor/plugin-contact-form/factor-settings.js").default
 files["cwd"] = require("../src/factor-settings.js").default
 module.exports = files
