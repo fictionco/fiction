5fd1a18697e504ef2fe64d58395a676c3c3b503e
Andrew Powers <arpowers@gmail.com>
2019-08-29 16:02:23 -0600

    live changes

---
+++ @apps/docs/src/home/figure-live-changes.vue
@@ -0,0 +1,102 @@
+<template>
+  <figure ref="wrapper" class="live-changes">
+    <div class="stage-wrap" :style="{transform: `scale(${scale})`}">
+      <div class="stage">
+        <div class="annotation">
+          <img src="./img/live-changes.svg" >
+        </div>
+        <div class="screenshot theme">
+          <img src="./img/theme.svg" >
+        </div>
+        <div class="screenshot edit-post">
+          <img src="./img/post.svg" >
+        </div>
+      </div>
+    </div>
+  </figure>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      width: 500
+    }
+  },
+  computed: {
+    scale() {
+      return this.width / 500
+    }
+  },
+  mounted() {
+    this.width = this.$refs.wrapper.clientWidth
+
+    window.addEventListener("resize", () => {
+      this.width = this.$refs.wrapper.clientWidth
+    })
+  }
+}
+</script>
+
+<style lang="less">
+figure.live-changes {
+  --color-wire: #e6ebf1;
+  --color-shaded: #ced2dd;
+  max-width: 100%;
+  .stage-wrap {
+    transform-origin: center left;
+  }
+  .stage {
+    padding: 30% 0;
+    width: 500px;
+    position: relative;
+    transform: translate(-120px, -50px) scale(1.1);
+    .annotation {
+      position: absolute;
+      top: 0;
+      left: 15%;
+      z-index: 10;
+      img {
+        width: 300px;
+      }
+    }
+    .screenshot {
+      top: 35px;
+      background: #fff;
+      display: inline-block;
+      //box-shadow: 0 6px 12px -2px rgba(50, 50, 93, 0.25),
+      //0 3px 7px -3px rgba(0, 0, 0, 0.3);
+      img {
+        max-width: 100%;
+        // filter: drop-shadow(0 4px 4px rgba(0, 0, 0, 0.1));
+      }
+      &.theme {
+        position: absolute;
+        right: 0;
+        padding: 5px;
+        transform: scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg)
+          rotate(2deg);
+        box-shadow: 1px 1px 4px 0 rgba(26, 26, 67, 0.1),
+          19px 25.5px 115px -25px rgba(50, 50, 93, 0.3),
+          13.4px 25.5px 75px -37.5px rgba(0, 0, 0, 0.3);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+      &.edit-post {
+        position: absolute;
+        top: 110px;
+        left: 0;
+        padding: 5px;
+        transform: scale(1) perspective(1040px) rotateY(11deg) rotateX(-2deg)
+          rotate(-2deg);
+        box-shadow: 1px 1px 4px 0 rgba(26, 26, 67, 0.1),
+          -19px 32.5px 125px -25px rgba(50, 50, 93, 0.3),
+          13.4px 37.5px 75px -37.5px rgba(0, 0, 0, 0.3);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+    }
+    perspective: 1000px;
+  }
+}
+</style>
