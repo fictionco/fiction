f0184b7ce265d9c618f36351ede6af707b64ec3e
Andrew Powers <arpowers@gmail.com>
2019-09-04 00:30:44 -0600

    quotes and themes

--- @apps/docs/src/home/figure-themes.vue
+++ @apps/docs/src/home/figure-themes.vue
@@ -2,8 +2,8 @@
   <figure ref="wrapper" class="themes-figure">
     <div class="stage-wrap" :style="{transform: `scale(${scale})`}">
       <div class="stage">
-        <div class="plugin-icon">
-          <img src="./img/icon-google-analytics.svg" >
+        <div v-for="(screenshot, i) in screenshots" :key="i" class="screenshot-wrap">
+          <img :src="screenshot.img" >
         </div>
       </div>
     </div>
@@ -14,20 +14,36 @@
 export default {
   data() {
     return {
-      width: 500
+      width: 500,
+      screenshots: [
+        {
+          img: require("./img/screenshot.jpg")
+        },
+        {
+          img: require("./img/screenshot.jpg")
+        },
+        {
+          img: require("./img/screenshot.jpg")
+        }
+      ]
     }
   },
   computed: {
     scale() {
-      return this.width / 500
+      return Math.max(Math.min(this.width / 500, 1), 0.5)
     }
   },
   mounted() {
-    this.width = this.$refs.wrapper.clientWidth
+    this.width = this.getWidth()
 
     window.addEventListener("resize", () => {
-      this.width = this.$refs.wrapper.clientWidth
+      this.width = this.getWidth()
     })
+  },
+  methods: {
+    getWidth() {
+      return this.$refs.wrapper ? this.$refs.wrapper.clientWidth : 100
+    }
   }
 }
 </script>
@@ -42,15 +58,47 @@ figure.themes-figure {
     padding: 30% 0;
     width: 500px;
     position: relative;
-    transform: translate(80px, -50px) scale(1.1);
+    transform: translateX(3em);
     @media (max-width: 767px) {
       transform: translate(-0, -0px);
     }
 
-    .plugin-icon {
-      background: blue;
+    perspective: 800px;
+
+    .screenshot-wrap {
+      width: 550px;
+      top: 0;
+      left: 0;
+      position: absolute;
+
+      border-radius: 8px;
+      box-shadow: 1px 1px 4px 0 rgba(26, 26, 67, 0.1),
+        -19px 32.5px 105px -5px rgba(50, 50, 93, 0.3),
+        13.4px 37.5px 55px -37.5px rgba(0, 0, 0, 0.3);
+      overflow: hidden;
+      transform-origin: 0 0;
+      img {
+        width: 100%;
+        display: block;
+      }
+      &:nth-child(1) {
+        transform: rotateX(4deg) rotateY(-15deg) translateZ(0em);
+        z-index: 10;
+      }
+      &:nth-child(2) {
+        left: 2em;
+
+        z-index: 9;
+        transform: rotateX(4deg) rotateY(-15deg) translateZ(-8em)
+          translateY(1rem) translateX(1rem);
+      }
+      &:nth-child(3) {
+        left: 4em;
+        z-index: 8;
+        transform: rotateX(4deg) rotateY(-15deg) translateZ(-16em)
+          translateY(2rem) translateX(2rem);
+      }
     }
-    perspective: 1000px;
   }
 }
 </style>
