e862df3dc1d4a935c94b85f40879deddba3b4197
Raymond <hello@finestpixels.com>
2019-09-06 17:36:41 -0600

    fiction vip icons

--- @apps/fiction/src/view-home-2/figure-vip.vue
+++ @apps/fiction/src/view-home-2/figure-vip.vue
@@ -37,7 +37,7 @@ export default {
     return {
       vipicons: [
         {
-          icon: "powered"
+          icon: "seo"
         },
         {
           icon: "design"
@@ -58,7 +58,7 @@ export default {
           icon: "support"
         },
         {
-          icon: "powered"
+          icon: "infrastructure2"
         }
       ]
     }
