cddd5894ce2a7f17264e3d17542f7829bfa424b0
Andrew Powers <arpowers@gmail.com>
2019-09-11 17:13:30 -0600

    home fixes

--- @apps/fiction/src/view-home/page-home.vue
+++ @apps/fiction/src/view-home/page-home.vue
@@ -11,28 +11,16 @@
               and grow your web app fast.
             </p>
 
-            <a href="/vip#contact">
-              <app-btn path="/vip#contact" btn="primary" size="large">
-                Contact Sales
-                <i class="fa fa-arrow-right" />
-              </app-btn>
-            </a>
+            <div class="actions">
+              <factor-email-list list-id="alphaProgram" />
+            </div>
           </div>
           <div class="graphic">
-            <img
-              class="img-dashboard"
-              :src="require(`./img/dashboard.jpg`)"
-              alt="Factor Framework"
-            />
-            <img
-              class="img-theme"
-              :src="require(`./img/theme-alpha.jpg`)"
-              alt="Factor Theme Alpha"
-            />
+            <img class="img-dashboard" :src="require(`./img/dashboard.jpg`)" alt="Factor Framework" >
+            <img class="img-theme" :src="require(`./img/theme-alpha.jpg`)" alt="Factor Theme Alpha" >
           </div>
         </div>
       </div>
-      <div class="hhh" />
     </section>
 
     <section class="home-features">
@@ -47,11 +35,7 @@
 
         <div class="feature">
           <div class="illustration">
-            <img
-              class="img-left"
-              :src="require(`./img/theme-ultra.png`)"
-              alt="Factor Theme Ultra"
-            />
+            <img class="img-left" :src="require(`./img/theme-ultra.png`)" alt="Factor Theme Ultra" >
           </div>
           <div class="content">
             <h3 class="title">Start — your web app</h3>
@@ -68,11 +52,7 @@
 
         <div class="feature feature-reverse">
           <div class="illustration">
-            <img
-              class="img-right"
-              :src="require(`./img/theme-alpha.jpg`)"
-              alt="Factor Theme Alpha"
-            />
+            <img class="img-right" :src="require(`./img/theme-alpha.jpg`)" alt="Factor Theme Alpha" >
           </div>
           <div class="content">
             <h3 class="title">Build — with one platform</h3>
@@ -89,7 +69,7 @@
 
         <div class="feature">
           <div class="illustration">
-            <img class="img-left" :src="require(`./img/dashboard.jpg`)" alt="Dashboard" />
+            <img class="img-left" :src="require(`./img/dashboard.jpg`)" alt="Dashboard" >
           </div>
           <div class="content">
             <h3 class="title">Manage — smarter, grow fast</h3>
@@ -107,44 +87,12 @@
         </div>
       </div>
     </section>
-
-    <!-- <section class="promo-wrap mast">
-      <div class="promo">
-        <factor-link path="https://factor.dev" class="item">
-          <h3>
-            Get Started
-            <br >with
-            <strong>Factor</strong>
-          </h3>
-          <pre><code class="hljs language-bash">$ yarn factor dev</code></pre>
-        </factor-link>
-
-        <factor-link path="/blog" class="item">
-          <h3>
-            <strong>Essays</strong> by Fiction
-          </h3>
-          <p>Essays on web development, remote-life and web business.</p>
-        </factor-link>
-
-        <factor-link path="/careers" class="item">
-          <h3>
-            Work With
-            <strong>Fiction</strong>
-          </h3>
-          <p>Join a talented team working on big ideas.</p>
-        </factor-link>
-      </div>
-    </section>-->
-
-    <el-to-top />
   </div>
 </template>
 
 <script>
 export default {
-  components: {
-    "el-to-top": () => import("../el/el-to-top")
-  },
+  components: {},
   data() {
     return {
       loading: true
@@ -164,18 +112,23 @@ export default {
     }
   },
   routeClass() {
-    return ["nav-light"]
+    return ["nav-white"]
   }
 }
 </script>
 <style lang="less">
 .page-home {
+  .email-list-form {
+    font-size: 1.3em;
+    max-width: 600px;
+    margin: 0 auto;
+  }
   .mast {
     margin: 0 auto;
     max-width: var(--max-width);
-    padding: 0 2em;
+    padding: 4em;
     @media (max-width: 767px) {
-      padding: 0 2em;
+      padding: 2em 2em;
     }
   }
   .intro-wrap {
@@ -194,7 +147,7 @@ export default {
       z-index: 1;
       display: grid;
       grid-template-columns: 4fr 3fr;
-      grid-gap: 2em;
+      grid-gap: 4em;
       align-items: center;
 
       @media (max-width: 767px) {
@@ -205,7 +158,7 @@ export default {
         .title {
           margin: 0.3em 0;
           font-size: 4em;
-          font-weight: 800;
+          font-weight: 600;
           letter-spacing: -0.03em;
           line-height: 1.1;
           @media (max-width: 767px) {
@@ -246,14 +199,15 @@ export default {
           border-radius: 0.5em;
           box-shadow: 39px 62.5px 125px -25px rgba(77, 77, 109, 0.1),
             23.4px 37.5px 75px -37.5px rgba(0, 0, 0, 0.4);
-          transform: translateX(0px) translateY(0) perspective(1190px) rotateY(-7deg)
-            rotateX(2deg) rotate(0.7deg);
+          transform: translateX(0px) translateY(0) perspective(1190px)
+            rotateY(-7deg) rotateX(2deg) rotate(0.7deg);
           @media (max-width: 767px) {
             max-width: 100%;
-            transform: translateX(0) translateY(0) perspective(0) rotateY(0) rotateX(0)
-              rotate(0);
+            transform: translateX(0) translateY(0) perspective(0) rotateY(0)
+              rotateX(0) rotate(0);
             box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
-              0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+              0 18px 36px -18px rgba(0, 0, 0, 0.3),
+              0 -12px 36px -8px rgba(0, 0, 0, 0.025);
           }
         }
         .img-theme {
@@ -265,7 +219,8 @@ export default {
           width: 400px;
           max-width: 100%;
           border-radius: 0.5em;
-          transform: translate(0) rotate3d(-5.5, 4.866, -0.5, -10deg) rotate(-1deg);
+          transform: translate(0) rotate3d(-5.5, 4.866, -0.5, -10deg)
+            rotate(-1deg);
           box-shadow: 39px 62.5px 125px -25px rgba(77, 77, 109, 0.1),
             23.4px 37.5px 75px -37.5px rgba(0, 0, 0, 0.4);
           @media (max-width: 960px) {
@@ -300,14 +255,12 @@ export default {
         font-weight: 800;
         font-size: 2.625em;
         line-height: 1.25em;
-        color: #1b223c;
       }
       .subtitle {
         margin: 20px auto 14px;
         font-weight: 400;
         font-size: 1.19em;
         line-height: 1.7em;
-        color: #525f7f;
       }
     }
     .feature {
@@ -351,78 +304,9 @@ export default {
           font-size: 1.6em;
           font-weight: 800;
           margin: 0 0 10px;
-          color: #1b223c;
         }
       }
     }
   }
-
-  // .promo-wrap {
-  //   position: relative;
-  //   z-index: 1;
-  //   .promo {
-  //     display: grid;
-  //     grid-template-columns: repeat(3, 1fr);
-  //     grid-gap: 2em;
-  //     margin-top: 2em;
-  //     @media (max-width: 767px) {
-  //       grid-template-columns: 1fr;
-  //     }
-  //     .item {
-  //       box-shadow: var(--panel-shadow);
-  //       color: inherit;
-  //       font-size: 1.9em;
-  //       line-height: 1.1em;
-  //       padding: 1em;
-  //       //min-height: 200px;
-  //       border-radius: 0.5rem;
-  //       background: linear-gradient(#fff, #f7f7f7);
-  //       background-size: cover;
-  //       background-position: 50%;
-  //       p {
-  //         font-size: 0.7em;
-  //         line-height: 1.3;
-  //         margin-top: 5px;
-  //         opacity: 0.9;
-  //       }
-  //       &:nth-child(1) {
-  //         display: flex;
-  //         align-items: flex-end;
-  //         flex-direction: column;
-  //         justify-content: center;
-  //         //text-align: right;
-  //         color: #fff;
-  //         text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.5);
-  //         background-image: url(./img/code.jpg),
-  //           linear-gradient(-126.24deg, #506677 4.42%, #2b3034 70.27%);
-
-  //         h3 {
-  //           margin-bottom: 10px;
-  //         }
-  //         pre code {
-  //           font-size: 0.7em;
-  //           padding: 8px 16px;
-  //           background: rgba(0, 0, 0, 0.5);
-  //           border-radius: 8px;
-  //           box-shadow: 0px 0px 5px rgba(4, 149, 254, 0.25);
-  //           color: #fff;
-  //         }
-  //       }
-  //       &:nth-child(2) {
-  //         text-align: center;
-  //         text-shadow: 0 1px 0px rgba(255, 255, 255, 0.5);
-  //         background-image: url(./img/blog.jpg);
-  //         padding-top: 85px;
-  //       }
-  //       &:nth-child(3) {
-  //         color: #fff;
-  //         text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.5), 0 0 15px rgba(0, 0, 0, 1);
-  //         text-align: center;
-  //         background-image: url(./img/team.jpg);
-  //         padding-top: 25px;
-  //       }
-  //     }
-  //   }
-  // }
 }
 </style>
