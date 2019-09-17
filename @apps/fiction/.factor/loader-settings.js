649abbaa2a3952b68b78b9147eefac762bf60491
Andrew Powers <arpowers@gmail.com>
2019-09-17 01:15:46 -0600

    dashboard tweaks

--- @apps/fiction/.factor/loader-settings.js
+++ @apps/fiction/.factor/loader-settings.js
@@ -5,6 +5,7 @@ files["contactForm"] = require("@factor/plugin-contact-form/factor-settings.js")
 files["emailList"] = require("@factor/plugin-email-list/factor-settings.js").default
 files["highlightCode"] = require("@factor/plugin-highlight-code/factor-settings.js").default
 files["jobs"] = require("@factor/plugin-jobs/factor-settings.js").default
+files["templates"] = require("@factor/post-page-templates/factor-settings.js").default
 files["uiComponentsStandard"] = require("@factor/ui-components-standard/factor-settings.js").default
 files["cwd"] = require("../src/factor-settings.js").default
 module.exports = files
