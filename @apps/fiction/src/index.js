4779075e4f2274176b1a15c29ae7366fb3d2fe70
Andrew Powers <arpowers@gmail.com>
2019-09-30 14:37:09 -0600

    slack notification

--- @apps/fiction/src/index.js
+++ @apps/fiction/src/index.js
@@ -2,12 +2,22 @@ module.exports.default = Factor => {
   return new (class {
     constructor() {
       this.addPaths()
-      if (Factor.FACTOR_TARGET == "app") {
-        this.addComponents()
-      }
+      this.events()
     }
 
-    addComponents() {}
+    events() {
+      // Track email sign up events
+      Factor.$events.$on("email-list-new-email-added", ({ email, listId }) => {
+        Factor.$http.request({
+          method: "post",
+          url:
+            "https://hooks.slack.com/services/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",
+          data: {
+            text: `New email [${email}] added to [${listId}]`
+          }
+        })
+      })
+    }
 
     async addPaths() {
       Factor.$filters.add("page-templates", _ => {
