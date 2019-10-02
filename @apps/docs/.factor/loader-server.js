807cd3481c85d02e09142a7e1c062ae1dbcd2aae
Raymond <hello@finestpixels.com>
2019-10-02 10:58:01 -0700

    fiction vip

--- @apps/docs/.factor/loader-server.js
+++ @apps/docs/.factor/loader-server.js
@@ -30,6 +30,7 @@ files["userServer"] = require("@factor/core-user/server").default
 files["userEmailsServer"] = require("@factor/core-user-emails/server").default
 files["userRolesServer"] = require("@factor/core-user-roles/server").default
 files["webpack"] = require("@factor/build-webpack").default
+files["appShared"] = require("@factor/theme-shared").default
 files["docs"] = require("@factor/theme-docs").default
 files["cwd"] = require("../src/index.js").default
 module.exports = files
