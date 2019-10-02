3d12e8c336ed1fefc9cc91cd9e1c189d683ca1ad
Raymond <hello@finestpixels.com>
2019-10-02 10:54:45 -0700

    fiction vip start

---
+++ @apps/fiction/src/v-vip-2/figure-sketch.vue
@@ -0,0 +1,81 @@
+<template>
+  <figure ref="wrapper" class="factor-figure">
+    <div class="sketches">
+      <div class="sketch intro">
+        <img src="./img/sketch-intro.svg" />
+      </div>
+      <div class="sketch boxes">
+        <img src="./img/sketch-boxes.svg" />
+      </div>
+      <div class="sketch feature">
+        <img src="./img/sketch-feature.svg" />
+      </div>
+    </div>
+  </figure>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="less">
+figure.factor-figure {
+  //perspective: 2000px;
+  position: relative;
+
+  .sketches {
+    .sketch {
+      max-width: 500px;
+      z-index: 10;
+      //transform-origin: top center;
+      background: #ffffff;
+      box-shadow: 0px 5px 15px rgba(27, 34, 60, 0.1), 0px 15px 35px rgba(27, 34, 60, 0.1),
+        0px 50px 100px rgba(27, 34, 60, 0.1);
+      border-radius: 4px;
+      overflow: hidden;
+      img {
+        width: 100%;
+      }
+      &.intro {
+        padding: 2rem 2rem 0;
+        transform: translateZ(-10px) translateX(0) translateY(3rem);
+        width: 80%;
+      }
+      &.feature {
+        padding: 1.5rem;
+        max-width: 150px;
+        transform: translateZ(-10px) translateX(24rem) translateY(-15rem);
+      }
+      &.boxes {
+        padding: 1.5rem;
+        position: absolute;
+        bottom: 0em;
+        left: 8em;
+      }
+    }
+    // @media (max-width: 900px) {
+    //   min-height: 250px;
+    //   margin: 3em 0;
+    //   .sketch {
+    //     &.feature {
+    //       transform: scale(0.85) translateX(-2em);
+    //       top: -2em;
+    //       right: auto;
+    //       bottom: auto;
+    //       left: 0;
+    //       box-shadow: 0 4px 35px 0 rgba(23, 55, 87, 0.1), 0 5px 15px 0 rgba(0, 0, 0, 0.07);
+    //     }
+    //     &.intro {
+    //       transform: scale(0.85) translateX(2em);
+    //       top: 3em;
+    //       right: 0;
+    //       bottom: auto;
+    //       left: auto;
+    //     }
+    //   }
+    // }
+  }
+}
+</style>
---
+++ @apps/fiction/src/v-vip-2/figure-wireframe.vue
@@ -0,0 +1,86 @@
+<template>
+  <figure ref="wrapper" class="factor-figure">
+    <div class="wireframes">
+      <div class="wireframe steps">
+        <img src="./img/wireframe-process.svg" />
+      </div>
+      <div class="wireframe intro">
+        <img src="./img/wireframe-intro.svg" />
+      </div>
+    </div>
+  </figure>
+</template>
+
+<script>
+//import pageUtils from "./utils"
+export default {
+  components: {}
+}
+</script>
+
+<style lang="less">
+//#feature-factor {
+//   .feature-figure-container {
+//     align-self: center;
+//     height: auto;
+//     min-width: 0;
+//   }
+//   @media (max-width: 900px) {
+//     .feature-figure-container {
+//       transform-origin: center center;
+//       figure {
+//         transform: none;
+//       }
+//     }
+//   }
+// }
+figure.factor-figure {
+  //perspective: 2000px;
+  position: relative;
+  //transform: translateX(-3rem);
+
+  .wireframes {
+    padding: 4rem 0;
+    .wireframe {
+      max-width: 500px;
+      z-index: 10;
+      //transform-origin: top center;
+      background: #ffffff;
+      box-shadow: 0px 5px 15px rgba(27, 34, 60, 0.1), 0px 15px 35px rgba(27, 34, 60, 0.1),
+        0px 50px 100px rgba(27, 34, 60, 0.1);
+      border-radius: 4px;
+      overflow: hidden;
+      img {
+        width: 100%;
+      }
+      &.steps {
+        padding: 2rem 2rem 0;
+        transform: translateZ(-10px) translateX(0rem) translateY(-2rem);
+        width: 80%;
+      }
+      &.intro {
+        padding: 0;
+        //transform: translateZ(-10px) translateX(9rem) translateY(-7rem);
+        width: 80%;
+        z-index: 11;
+        position: absolute;
+        bottom: 0em;
+        left: 8em;
+      }
+    }
+    // @media (max-width: 900px) {
+    //   min-height: 250px;
+    //   margin: 3em 0;
+    //   .sketch {
+    //     &.intro {
+    //       transform: scale(0.85) translateX(2em);
+    //       top: 3em;
+    //       right: 0;
+    //       bottom: auto;
+    //       left: auto;
+    //     }
+    //   }
+    // }
+  }
+}
+</style>
