ebb3a168f8380e0bcbc44db450bacdeaddbf76f8
Raymond <hello@finestpixels.com>
2019-10-04 16:50:16 -0700

    start new plugins page

---
+++ @apps/docs/src/plugins/section-plugins.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="section-plugins">
+    <section
+      v-for="(post, index) in posts"
+      :id="post.id"
+      :key="index"
+      class="post"
+      :class="post.class"
+    >
+      <div class="post-image">
+        <home-icon icon="dashboard" />
+      </div>
+      <div class="post-content">
+        <h3 class="title">{{ post.title }} Google Analytics</h3>
+        <div class="meta">
+          <span class="author">by Factor</span> in
+          <span class="category">Search</span>
+          <span class="download-count">1,262 downloads</span>
+        </div>
+        <p class="text">{{ post.text }} Easily add Google Analytics to your Factor site.</p>
+      </div>
+    </section>
+
+    <!-- <section
+      v-for="(feature, index) in features"
+      :id="feature.id"
+      :key="index"
+      class="features content"
+      :class="[index == features.length - 1 ? 'last' : '']"
+    >
+      <div class="split-feature" :class="[index % 2 == 0 ? 'even' : 'odd']">
+        <div class="feature-content-container">
+          <div class="feature-content">
+            <home-icon v-if="feature.icon" class="feature-icon" :icon="feature.icon" />
+            <h2 class="title">{{ feature.title }}</h2>
+            <div class="text">{{ feature.text }}</div>
+            <div v-if="feature.link" class="action">
+              <factor-link :path="feature.link.path">{{ feature.link.text }} &rarr;</factor-link>
+            </div>
+          </div>
+        </div>
+        <div class="feature-figure-container">
+          <div v-if="feature.figure" class="figure-container">
+            <component :is="feature.figure" />
+          </div>
+        </div>
+      </div>
+    </section>-->
+
+    <!-- <div class="interstitial-cta">
+      <div class="text">
+        <h2 class="title">Create and Submit Your Plugin</h2>
+        <h2
+          class="title-sub"
+        >Learn about extension development and how to submit your extension to the Factor library.</h2>
+      </div>
+
+      <div class="buttons">
+        <factor-link btn="primary" path="/guide/">Submit a Plugin &rarr;</factor-link>
+        <factor-link btn="default" path="/guide/create-plugins">Create a Plugin &rarr;</factor-link>
+      </div>
+    </div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    "home-icon": () => import("./icon.vue")
+  },
+  data() {
+    return {
+      posts: [
+        {
+          icon: "powered",
+          title: "Powered by VueJS and MongoDB",
+          text: `When you use Factor you'll be using best-of-class open source software under the hood. 
+            Stop worrying about lock-in and immature tech. No more endlessly comparing new tools and techniques. 
+            Just trust Factor and get started building your app.`,
+          //figure: () => import("./figure-powered-by.vue"),
+          link: { path: "/guide/quickstart", text: "View Quickstart" }
+        },
+        {
+          icon: "ssr",
+          title: "Dynamic Beats Static",
+          text: `Factor is a universal Javascript framework which means you can dynamically make changes
+              to your content and see them reflected immediately (without a build step). 
+              This also enables custom endpoints and server-side rendering (SSR) important for SEO, 
+              social, and performance reasons.`,
+          link: { path: "/guide", text: "Try Factor" }
+        },
+        {
+          icon: "dashboard2",
+          title: "Dashboard Included",
+          text: `Factor comes with a professional dashboard and post management system.
+            This tool was carefully crafted to give you maximum powerful but with minimum bloat. 
+            It is simple by default but can be extended to handle even the most complext tasks.`,
+          link: { path: "/guide", text: "Learn More" }
+        },
+        {
+          id: "plugins-feature",
+          //icon: "plugins",
+          title: `Plugins that just work`,
+          text: `Most Javascript frameworks make you do way too much coding and customization 
+              to make plugins work. That's why Factor makes plugins dead simple with intelligent 
+              defaults and no mandatory customization.`,
+          link: { path: "/plugins", text: "View Plugins" }
+        },
+        {
+          //icon: "brush",
+          title: "Theming for the 21st Century",
+          text: `Ever seen a theming system for Javascript apps that you could work with? We hadn't either. 
+              Factor was developed from the start with customizeable theming and rapid app development in mind.`,
+          link: { path: "/themes", text: "View Themes" }
+        }
+      ]
+    }
+  },
+  methods: {}
+}
+</script>
+<style lang="less">
+.section-plugins {
+  .post {
+    padding: 1rem;
+    margin-bottom: 1rem;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-gap: 2rem;
+    background: #fff;
+    border-radius: 6px;
+    border: 1px solid #e8ebed;
+    transition: 0.29s cubic-bezier(0.52, 0.01, 0.16, 1);
+    &:hover {
+      box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.07), 0px 18px 26px rgba(80, 102, 119, 0.16);
+      transform: translateY(-0.4rem);
+    }
+    .title {
+      font-weight: 500;
+      font-size: 1.2em;
+      line-height: 1.5em;
+    }
+    .text {
+      font-size: 0.94em;
+      line-height: 1.7em;
+      opacity: 0.85;
+    }
+    h3,
+    p {
+      margin-bottom: 10px;
+    }
+    p:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  // .interstitial-cta {
+  //   display: grid;
+  //   grid-template-columns: 1fr auto;
+  //   align-items: center;
+  //   border-top: 2px solid #f6f9fc;
+  //   margin: 60px -20px 0;
+  //   padding: 60px 20px 0;
+  //   @media (max-width: 767px) {
+  //     grid-template-columns: 1fr;
+  //   }
+  //   .text h2 {
+  //     &.title {
+  //       color: var(--color-primary);
+  //     }
+  //     font-size: 1.5em;
+  //   }
+  //   .buttons {
+  //     margin: 0 0 0 20px;
+  //     @media (max-width: 767px) {
+  //       margin: 20px 0 0 0;
+  //     }
+  //   }
+  // }
+}
+</style>
---
+++ @apps/docs/src/plugins/v-plugins.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="view-plugins">
+    <section class="header">
+      <div class="content-pad">
+        <div class="header-content">
+          <h1 class="page-title">Factor Plugin Library</h1>
+          <h3 class="page-title-sub">Extend your project features and do more with Factor.</h3>
+        </div>
+        <div class="header-figure">
+          <figure-plugins />
+        </div>
+      </div>
+    </section>
+
+    <div class="plugins-container content-pad">
+      <div class="content">
+        <section class="plugins-featured">
+          <header class="section-header">
+            <h1 class="title">Featured</h1>
+          </header>
+          <section-plugins />
+        </section>
+      </div>
+      <div class="sidebar">
+        <plugins-sidebar />
+        <section-plugins />
+      </div>
+    </div>
+    <h2>CTA will go here</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    "plugins-sidebar": () => import("./plugins-sidebar"),
+    "figure-plugins": () => import("./figure-plugins"),
+    "section-plugins": () => import("./section-plugins")
+  },
+  data() {
+    return {
+      loading: true
+    }
+  },
+  metaInfo() {
+    return {
+      title: "Factor Plugin Library",
+      description: "Extend your project features and do more with Factor."
+    }
+  },
+  routeClass() {
+    //return ["nav-light"]
+  }
+}
+</script>
+<style lang="less">
+.view-plugins {
+  padding-top: 45px;
+  font-weight: 400;
+  overflow: hidden;
+  background-color: #f6f9fc;
+  .content-pad {
+    max-width: 1100px;
+    margin: 0 auto;
+    padding: 0 1.5em;
+    width: 100%;
+    z-index: 10;
+    position: relative;
+  }
+
+  /* HEADER */
+  .header {
+    background-image: url("./img/dot.svg");
+    overflow: hidden;
+
+    .content-pad {
+      display: grid;
+      grid-template-columns: 4fr 3fr;
+    }
+
+    .header-content {
+      padding: 4em 0;
+
+      .page-title {
+        font-size: 3.5em;
+        line-height: 1.1;
+        font-weight: 500;
+        margin: 0 0 1rem;
+        letter-spacing: -0.03em;
+      }
+      .page-title-sub {
+        font-size: 1.6em;
+        opacity: 0.7;
+      }
+      @media (max-width: 900px) {
+        padding: 3rem 0;
+        .page-title {
+          font-size: 1.7em;
+          line-height: 1.3;
+        }
+        .page-title-sub {
+          font-size: 1.4em;
+          line-height: 1.1;
+        }
+      }
+    }
+
+    .header-figure {
+      position: relative;
+    }
+
+    @media (max-width: 900px) {
+      .content-pad {
+        grid-template-columns: 1fr;
+      }
+    }
+  }
+
+  /* PAGE CONTENT */
+  .plugins-container {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    grid-gap: 2rem;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+
+    .section-header {
+      margin-bottom: 2rem;
+      .title {
+        font-size: 2em;
+        font-weight: 600;
+        line-height: 1.1;
+        letter-spacing: -0.02em;
+
+        @media (max-width: 670px) {
+          font-size: 2em;
+          line-height: 1.2;
+        }
+      }
+    }
+
+    @media (max-width: 900px) {
+      grid-template-columns: 1fr;
+    }
+  }
+}
+</style>
