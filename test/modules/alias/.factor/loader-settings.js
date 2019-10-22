0eec432159b45dc7f93c793c9000874e39788909
Andrew Powers <arpowers@gmail.com>
2019-10-22 09:45:16 -0600

    test

--- test/modules/alias/.factor/loader-settings.js
+++ test/modules/alias/.factor/loader-settings.js
@@ -3,8 +3,6 @@ const cwd = process.env.FACTOR_CWD || process.cwd()
 let m = {}
 try {
   m = require(`${cwd}/.factor/loader-settings.js`)
-} catch (error) {
-  console.error(error)
-}
+} catch (error) {}
 
 module.exports = m
