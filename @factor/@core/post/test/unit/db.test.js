d57746130a25d97d6eaebf842eaf45bf4e2dbeb9
Andrew Powers <arpowers@gmail.com>
2019-10-28 11:44:35 -0600

    database test

--- @factor/@core/post/test/unit/db.test.js
+++ @factor/@core/post/test/unit/db.test.js
@@ -3,37 +3,34 @@ import Factor from "@factor/core"
 import { $DB, FactorDB } from "../../database"
 import mongoose from "mongoose"
 import { getModel } from "../../server"
-import { dirname } from "path"
 
-mongoose.connection = {
-  readyState: 0, // disconnected
-  close: jest.fn(() => Promise.resolve())
-}
+// mongoose.connection = {
+//   readyState: 0, // disconnected
+//   close: jest.fn(() => Promise.resolve())
+// }
 
 let db
 describe("db-utility", () => {
-  beforeAll(async () => {
-    db = new FactorDB()
-  })
+  beforeAll(async () => {})
 
   it("creates singleton", () => {
     expect($DB).toBeInstanceOf(FactorDB)
   })
 
   it("initializes DB", async () => {
-    await db.initialize()
+    jest.spyOn(mongoose, "connect")
+
+    await $DB.initialize()
 
     expect(mongoose.connect).toHaveBeenCalled()
   })
 
-  // it("loads models", async () => {
-  //   expect(db.__schemas.post).toBeTruthy()
-  //   expect(db.__models.post).toBeTruthy()
-  // })
+  it("loads models", async () => {
+    expect($DB.__schemas.post).toBeTruthy()
+    expect($DB.__models.post).toBeTruthy()
 
-  // it("getModel()", () => {
-  //   const postModel = db.model("post")
+    const postModel = $DB.model("post")
 
-  //   expect(postModel).toBe(getModel("post"))
-  // })
+    expect(postModel).toBe(getModel("post"))
+  })
 })
