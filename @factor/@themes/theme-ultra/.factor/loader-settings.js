663f375f2f81dac565a05f9b873445eafd632e33
Andrew Powers <arpowers@gmail.com>
2019-10-28 12:07:16 -0600

    move modules

--- @factor/@themes/theme-ultra/.factor/loader-settings.js
+++
@@ -1,7 +0,0 @@
-/******** GENERATED FILE ********/
-const files = {}
-files["contactForm"] = require("@factor/plugin-contact-form/factor-settings.js").default
-files["templates"] = require("@factor/post-page-templates/factor-settings.js").default
-files["uiComponentsStandard"] = require("@factor/ui-components-standard/factor-settings.js").default
-files["cwd"] = require("../src/factor-settings.js").default
-module.exports = files
