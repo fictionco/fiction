cf8fc4b2eaf4feb3e23bb08df2ea446e63cd7209
Andrew Powers <arpowers@gmail.com>
2019-10-29 00:18:20 -0600

    refactor ui packages

--- @factor/setup/index.js
+++ @factor/setup/index.js
@@ -7,161 +7,162 @@ import log from "@factor/logger"
 import { getExtensions } from "@factor/build/util"
 import { getPath } from "@factor/paths"
 import { sortPriority, deepMerge, applyFilters, addCallback } from "@factor/tools"
-export default Factor => {
-  return new (class {
-    constructor() {
-      this.configFile = getPath("config-file-public")
-      this.secretsFile = getPath("config-file-private")
-      addCallback("cli-setup", _ => this.runSetup(_))
 
-      addCallback("after-first-server-extend", () => {
-        this.setupNeeded = applyFilters("setup-needed", [])
-        if (this.setupNeeded.length > 0) {
-          let lines = this.setupNeeded.map(_ => {
-            return {
-              title: _.title,
-              value: "",
-              indent: true
-            }
-          })
-          if (process.env.FACTOR_COMMAND !== "setup") {
-            lines.push({ title: "Run 'yarn factor setup'", value: "" })
-          }
+export class FactorSetup {
+  constructor() {
+    this.configFile = getPath("config-file-public")
+    this.secretsFile = getPath("config-file-private")
+    addCallback("cli-setup", _ => this.runSetup(_))
 
-          log.formatted({
-            title: "Setup Needed",
-            lines,
-            color: "yellow"
-          })
-        }
-      })
-    }
-
-    // Setup entry. Give basic information and create an extensible select option for setup.
-    async runSetup(_arguments) {
-      const { inquirer } = _arguments
-      let answers
-
-      log.formatted({
-        title: "Welcome to Factor Setup!",
-        lines: [
-          { title: "Theme", value: this.extensionNames("theme"), indent: true },
-          {
-            title: "Modules",
-            value: this.extensionNames("plugin", "count"),
+    addCallback("after-first-server-extend", () => {
+      this.setupNeeded = applyFilters("setup-needed", [])
+      if (this.setupNeeded.length > 0) {
+        let lines = this.setupNeeded.map(_ => {
+          return {
+            title: _.title,
+            value: "",
             indent: true
           }
-        ]
-      })
-
-      let setups = applyFilters(
-        "cli-add-setup",
-        [
-          {
-            name: "Exit Setup",
-            value: "exit",
-            callback: async () => {
-              // eslint-disable-next-line unicorn/no-process-exit
-              process.exit()
-            },
-            priority: 1000
-          }
-        ],
-        this.existingSettings()
-      )
-
-      setups = sortPriority(setups)
-
-      // Escapes the endless loop
-      let askAgain = true
-
-      const ask = async () => {
-        answers = await inquirer.prompt({
-          type: "list",
-          name: `setupItem`,
-          message: `What would you like to do?`,
-          choices: setups.map(({ callback, ...keep }) => keep)
         })
-
-        console.log() // break
-
-        const setupRunner = setups.find(_ => _.value == answers.setupItem)
-
-        await setupRunner.callback(_arguments)
-
-        if (askAgain) {
-          await ask()
+        if (process.env.FACTOR_COMMAND !== "setup") {
+          lines.push({ title: "Run 'yarn factor setup'", value: "" })
         }
+
+        log.formatted({
+          title: "Setup Needed",
+          lines,
+          color: "yellow"
+        })
       }
+    })
+  }
 
-      await ask()
-    }
+  // Setup entry. Give basic information and create an extensible select option for setup.
+  async runSetup(_arguments) {
+    const { inquirer } = _arguments
+    let answers
 
-    existingSettings() {
-      if (!fs.pathExistsSync(this.configFile)) {
-        fs.writeJsonSync(this.configFile, { config: {} })
-      }
-      const publicConfig = require(this.configFile)
+    log.formatted({
+      title: "Welcome to Factor Setup!",
+      lines: [
+        { title: "Theme", value: this.extensionNames("theme"), indent: true },
+        {
+          title: "Modules",
+          value: this.extensionNames("plugin", "count"),
+          indent: true
+        }
+      ]
+    })
 
-      fs.ensureFileSync(this.secretsFile)
-      const privateConfig = envfile.parseFileSync(this.secretsFile)
+    let setups = applyFilters(
+      "cli-add-setup",
+      [
+        {
+          name: "Exit Setup",
+          value: "exit",
+          callback: async () => {
+            // eslint-disable-next-line unicorn/no-process-exit
+            process.exit()
+          },
+          priority: 1000
+        }
+      ],
+      this.existingSettings()
+    )
 
-      return { publicConfig, privateConfig }
-    }
+    setups = sortPriority(setups)
 
-    extensionNames(type, format = "join") {
-      const exts = getExtensions().filter(_ => _.extend == type)
+    // Escapes the endless loop
+    let askAgain = true
 
-      if (exts && exts.length > 0) {
-        const names = exts.map(_ => _.name)
-
-        return format == "count" ? names.length : names.join(", ")
-      } else return "none"
-    }
-
-    prettyJson(data) {
-      const highlight = require("cli-highlight").highlight
-      return highlight(require("json2yaml").stringify(data, null, "  "))
-    }
-
-    async writeConfig(file, values) {
-      if (!file || !values) {
-        return
-      }
-      let answers = await inquirer.prompt({
-        type: "confirm",
-        name: `writeFiles`,
-        message: `Write the following settings to the "${chalk.cyan(
-          file
-        )}" file? \n\n ${this.prettyJson(values)} \n`,
-        default: true
+    const ask = async () => {
+      answers = await inquirer.prompt({
+        type: "list",
+        name: `setupItem`,
+        message: `What would you like to do?`,
+        choices: setups.map(({ callback, ...keep }) => keep)
       })
 
-      console.log()
-      if (answers.writeFiles) {
-        this.writeFiles(file, values)
-        log.success(`Wrote to ${file}...\n\n`)
-      } else {
-        log.log(`Writing skipped.`)
-      }
-      console.log()
+      console.log() // break
 
+      const setupRunner = setups.find(_ => _.value == answers.setupItem)
+
+      await setupRunner.callback(_arguments)
+
+      if (askAgain) {
+        await ask()
+      }
+    }
+
+    await ask()
+  }
+
+  existingSettings() {
+    if (!fs.pathExistsSync(this.configFile)) {
+      fs.writeJsonSync(this.configFile, { config: {} })
+    }
+    const publicConfig = require(this.configFile)
+
+    fs.ensureFileSync(this.secretsFile)
+    const privateConfig = envfile.parseFileSync(this.secretsFile)
+
+    return { publicConfig, privateConfig }
+  }
+
+  extensionNames(type, format = "join") {
+    const exts = getExtensions().filter(_ => _.extend == type)
+
+    if (exts && exts.length > 0) {
+      const names = exts.map(_ => _.name)
+
+      return format == "count" ? names.length : names.join(", ")
+    } else return "none"
+  }
+
+  prettyJson(data) {
+    const highlight = require("cli-highlight").highlight
+    return highlight(require("json2yaml").stringify(data, null, "  "))
+  }
+
+  async writeConfig(file, values) {
+    if (!file || !values) {
       return
     }
+    let answers = await inquirer.prompt({
+      type: "confirm",
+      name: `writeFiles`,
+      message: `Write the following settings to the "${chalk.cyan(
+        file
+      )}" file? \n\n ${this.prettyJson(values)} \n`,
+      default: true
+    })
 
-    writeFiles(file, values) {
-      const { publicConfig, privateConfig } = this.existingSettings()
-
-      if (file.includes("factor-config")) {
-        const conf = deepMerge([publicConfig, values])
-        fs.writeFileSync(this.configFile, JSON.stringify(conf, null, "  "))
-      }
-
-      if (file.includes("env")) {
-        const sec = deepMerge([privateConfig, values])
-
-        fs.writeFileSync(this.secretsFile, envfile.stringifySync(sec))
-      }
+    console.log()
+    if (answers.writeFiles) {
+      this.writeFiles(file, values)
+      log.success(`Wrote to ${file}...\n\n`)
+    } else {
+      log.log(`Writing skipped.`)
     }
-  })()
+    console.log()
+
+    return
+  }
+
+  writeFiles(file, values) {
+    const { publicConfig, privateConfig } = this.existingSettings()
+
+    if (file.includes("factor-config")) {
+      const conf = deepMerge([publicConfig, values])
+      fs.writeFileSync(this.configFile, JSON.stringify(conf, null, "  "))
+    }
+
+    if (file.includes("env")) {
+      const sec = deepMerge([privateConfig, values])
+
+      fs.writeFileSync(this.secretsFile, envfile.stringifySync(sec))
+    }
+  }
 }
+
+export default new FactorSetup()
--- @factor/tools/index.js
+++ @factor/tools/index.js
@@ -8,6 +8,7 @@ export * from "./filters"
 export * from "./settings"
 export * from "./store-router"
 export * from "./markdown"
+export * from "./html"
 
 export { default as isNode } from "detect-node"
 export { default as guid } from "uniqid"
