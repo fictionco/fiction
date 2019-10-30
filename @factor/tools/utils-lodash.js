a19510efc3063d96ad76b0cff6ca59dce32b62ca
Andrew Powers <arpowers@gmail.com>
2019-10-30 14:52:36 -0600

    add orderby

--- @factor/tools/utils-lodash.js
+++ @factor/tools/utils-lodash.js
@@ -2,9 +2,10 @@ import isEqual from "lodash.isequal"
 import throttle from "lodash.throttle"
 import isEmpty from "lodash.isempty"
 import pickBy from "lodash.pickby"
+import orderBy from "lodash.orderby"
 import uniq from "lodash.uniq"
 import cloneDeep from "lodash.clonedeep"
 import merge from "lodash.merge"
 import identity from "lodash.identity"
 
-export { isEqual, throttle, isEmpty, uniq, pickBy, cloneDeep, merge, identity }
+export { isEqual, throttle, isEmpty, uniq, pickBy, orderBy, cloneDeep, merge, identity }
