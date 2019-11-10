d8c17d761f3d307024bf1b143fa25c0d05625a11
Andrew Powers <arpowers@gmail.com>
2019-11-09 20:55:43 -0500

    test: start app testing

--- @factor/app/test/unit/create-app.test.js
+++ @factor/app/test/unit/create-app.test.js
@@ -1,3 +1,15 @@
+import { createApp } from "@factor/app/app"
+
 describe("create-app", () => {
-  it.todo("creates vue application and returns correctly")
+  it("creates vue application and returns correctly", async () => {
+    const { app, router, store } = await createApp()
+
+    expect(app).toBeTruthy()
+    expect(router).toBeTruthy()
+    expect(store).toBeTruthy()
+  })
+
+  it("sets FACTOR_TARGET to app", () => {
+    expect(process.env.FACTOR_TARGET).toBe("app")
+  })
 })
--- @factor/app/test/unit/entry-browser.test.js
+++ @factor/app/test/unit/entry-browser.test.js
@@ -1,6 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import * as app from "@factor/app/app"
+
+import { waitFor, indexHtml } from "@test/utils"
+
+let spies = {}
 describe("browser-app", () => {
-  it.todo("enters application and mounts correctly")
-  it.todo("adds global variables to window (for reference)")
+  beforeAll(() => {
+    document.open()
+    document.write(indexHtml())
+    document.close()
+    spies.createApp = jest.spyOn(app, "createApp")
+  })
+  it("enters application and mounts correctly", async () => {
+    require("@factor/app/entry-browser")
+
+    await waitFor(100)
+
+    expect(spies.createApp).toHaveBeenCalled()
+  })
+
+  it("adds global variables to window (for reference)", async () => {
+    await waitFor(20)
+
+    expect(window.factorApp).toBeTruthy()
+
+    await waitFor(20)
+
+    expect(window.factorReady).toBeTruthy()
+  })
 
   it.todo("fires appropriate hooks")
 
