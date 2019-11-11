d839f31721fa4df1d2d7067b73af9c3a2c8241f6
Andrew Powers <arpowers@gmail.com>
2019-11-11 08:38:58 -0500

    fix: script

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -6,13 +6,13 @@ const msg = require("fs")
   .readFileSync(msgPath, "utf-8")
   .trim()
 
-const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(version|release|feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
   console.log()
   console.error(
     `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(
-      `invalid commit message format.`
+      `invalid message format for commit "${msg}"`
     )}\n\n` +
       chalk.red(
         `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
