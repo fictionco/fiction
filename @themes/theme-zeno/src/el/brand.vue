bcee1f21f209b8b34d6af4acdd50237dae90c1b2
finestpixels <ray@fiction.com>
2019-11-12 17:14:53 -0500

    fix: zeno html no custom classes

---
+++ @themes/theme-zeno/src/el/brand.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <factor-link path="/">
+      <component :is="setting(`site.logo`)" />
+    </factor-link>
+  </div>
+</template>
+<script>
+import { setting } from "@factor/tools"
+export default {
+  methods: { setting }
+}
+</script>
