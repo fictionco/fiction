063c04f05930906d58320e77d3a409ea07ff1046
Andrew Powers <arpowers@gmail.com>
2019-11-12 22:37:34 -0500

    test: attachment #2

--- test/modules/utils/mongod.js
+++ test/modules/utils/mongod.js
@@ -1,28 +1,26 @@
 import { MongoMemoryServer } from "mongodb-memory-server"
 import { dbInitialize, dbDisconnect } from "@factor/post/database"
 import { initializeEndpointServer } from "@factor/endpoint/server"
-import { createEndpointServer, closeServer } from "@factor/server"
+import { createMiddlewareServer, closeServer } from "@factor/server"
 let mongod
-export async function startEndpointTestingServer() {
+export async function startEndpointTestingServer({ port }) {
   mongod = new MongoMemoryServer()
 
-  const uri = await mongod.getConnectionString()
-  const port = await mongod.getPort()
+  const dbUrl = await mongod.getConnectionString()
+  const dbPort = await mongod.getPort()
   const dbPath = await mongod.getDbPath()
   const dbName = await mongod.getDbName()
 
-  process.env.DB_CONNECTION = uri
-
+  process.env.DB_CONNECTION = dbUrl
+  initializeEndpointServer()
+  await createMiddlewareServer({ port })
   await dbInitialize()
 
-  initializeEndpointServer()
-  await createEndpointServer({ port })
-
-  return { uri, port, dbPath, dbName }
+  return { dbUrl, dbPort, dbPath, dbName }
 }
 
 export async function stopEndpointTestingServer() {
   await mongod.stop()
   await dbDisconnect()
-  await closeServer()
+  closeServer()
 }
