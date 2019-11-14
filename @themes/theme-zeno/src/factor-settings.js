664f4a0cb92cea74a652cb27dafd61215a89e2f5
finestpixels <ray@fiction.com>
2019-11-13 19:12:24 -0500

    chore: zeno dynamic content

--- @themes/theme-zeno/src/factor-settings.js
+++ @themes/theme-zeno/src/factor-settings.js
@@ -24,36 +24,156 @@ export default {
       {
         path: "/#pricing",
         name: "Pricing"
-      }           
+      }
     ],
+  },
+  home: {
+    meta: {
+      title: "Factor Zeno Theme",
+      description:
+        "A minimal, personal or portfolio theme. Ideal for entrepreneurs or individuals of multiple creative professions."
+    },
+    intro: {
+      pretitle: "Grow Your Business, Not Infrastructure.",
+      title: "We build, optimize, secure, and support your cloud with no long-term contract.",
+      content: "Experts that architect and manage clouds with dedicated and obsessive 24/7/365 support.",
+      buttons: [
+        {
+          link: "/contact",
+          text: "Contact Us",
+          classes: "bg-green-300 text-indigo-600"
+        },
+        {
+          link: "/pricing",
+          text: "Pricing",
+          classes: "bg-gray-100 text-indigo-600"
+        },
+      ]
+    },
+    clients: {
+      title: "Working with the following clouds:",
+      list: [
+        {
+          link: "/",
+          image: require("./img/aws.svg"),
+          alt: "Amazon Web Services"
+        },
+        {
+          link: "/",
+          image: require("./img/google-cloud-platform.svg"),
+          alt: "Google Cloud Platform"
+        },
+        {
+          link: "/",
+          image: require("./img/digital-ocean.svg"),
+          alt: "Digital Ocean"
+        }
+      ]
+    },
+    solutions: {
+      title: "Solutions",
+      items: [
+        {
+          icon: require("./img/plan.svg"),
+          title: "Plan",
+          list: [
+            {
+              content: "Cloud infrastructure design that is secure, performant, elastic, affordable, and agnostic."
+            },
+            {
+              content: "Migrate existing infrastructure from Heroku, on-premise, or other providers."
+            },
+            {
+              content: "Re-architect your infrastructure or migrate to microservices, containers, and serverless."
+            }
+          ]
+        },
+        {
+          icon: require("./img/build.svg"),
+          title: "Build",
+          list: [
+            {
+              content: "From simple LAMP/MEAN stacks to complex multi-region architectures using Packer, Terraform, containerization and beyond, we can build it."
+            },
+            {
+              content: "CDN configuration and optimization including expert knowledge of CloudFlare."
+            },
+            {
+              content: "Local development environments, continuous integration pipelines, deployments, and testing infrastructure."
+            }
+          ]
+        },
+        {
+          icon: require("./img/manage.svg"),
+          title: "Manage",
+          list: [
+            {
+              content: "Manage your infrastructure spend and reduce costs."
+            },
+            {
+              content: "Infrastructure monitoring and alerting using native cloud provider offerings or third parties such as Datadog, Pingdom, and PagerDuty."
+            },
+            {
+              content: "Compliance and governance. Security patching and updating of infrastructure components."
+            }
+          ]
+        }
+      ]
+    },
+    devops: {
+      pretitle: "Devops-as-a-Service",
+      title: "Your first and last DevOps hire!",
+      content: "<p>By leveraging our decades of DevOps and sysadmin experience, we'll help you focus on what's important… Growing your business.</p><p>Elastic Byte will handle uptime, security, cost optimization, disaster recovery, and performance. We’re always on-call, ready to assist. Focused on being responsive, transparent, and thorough.</p>",
+      buttons: [
+        {
+          link: "/contact",
+          text: "Contact Us",
+          classes: "bg-green-300 text-indigo-600"
+        }
+      ],
+      figure: () => import("./el/figure-devops.vue")
+    },
+    infrastructure: {
+      pretitle: "Infrastructure as Code",
+      title: "Packer + Terraform = ",
+      titleIcon: require("./img/custom-heart.svg"),
+      items: [
+        {
+          image: require("./img/logo-packer.svg"),
+          alt: "HashiCorp Packer",
+          content: "<p>Packer automates the creation of machine images across cloud providers.</p><p>It embraces modern configuration management by utilizing automated scripts to install and configure software within images.</p><p>Produces exact point-in-time images with multi-region replication support.</p>",
+        },
+        {
+          image: require("./img/logo-terraform.svg"),
+          alt: "HashiCorp Terraform",
+          content: "<p>Terraform codifies infrastructure and resources, replacing manual point and click with a simple and intuitive declarative configuration.</p><p>Confidently apply reproducible infrastructure transformations. Humans make mistakes and forget details, machines and code don't.</p><p>Terraform configurations can be stored in version control, shared, and collaborated on by teams.</p>",
+        }
+      ],
+      syntaxTitle: "terraform.tf",
+      syntax: () => import("./el/figure-infrastructure.vue")
+    },
+    testimonials: {
+      pretitle: "Success Stories",
+      title: "Our customers love what we do!",
+      items: [
+        {
+          content: "@elasticbyte architected and fully managed our migration to #kubernetes and on-premises enterprise deployments. They've been absolutely fantastic!",
+          image: require("./img/client.svg"),
+          author: "Will Norton",
+          info: "CEO, SimplyAgree"
+        },
+        {
+          content: "@elasticbyte architected and fully managed our migration to #kubernetes and on-premises enterprise deployments. They've been absolutely fantastic!",
+          image: require("./img/client.svg"),
+          author: "Will Norton",
+          info: "CEO, SimplyAgree"
+        }
+      ]
+    },
   },
   footer: {
     headline: "Released under the GPL-2 License",
     legal: "Copyright &copy; - <a href='https://www.fiction.com'>Fiction.com</a>",
     logo: () => import("./el/logo-fiction.vue")
-  },
-  home: {
-    clients: [
-      {
-        link: "/",
-        image:  require("./img/aws.svg"),
-        alt: "Amazon Web Services"
-      },
-      {
-        link: "/",
-        image:  require("./img/aws.svg"),
-        alt: "Amazon Web Services"
-      },
-      {
-        link: "/",
-        image:  require("./img/aws.svg"),
-        alt: "Amazon Web Services"
-      }
-    ],
-    meta: {
-      title: "Factor Zeno Theme",
-      description:
-        "A minimal, personal or portfolio theme. Ideal for entrepreneurs or individuals of multiple creative professions."
-    }
   }
 }
