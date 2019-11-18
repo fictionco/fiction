b231d6587528ef11f0ef10c04ccbfbae94e1ad9b
Andrew Powers <arpowers@gmail.com>
2019-11-18 10:58:14 +0100

    chore: docs

--- global.d.ts
+++ global.d.ts
@@ -14,3 +14,15 @@ interface Window {
 }
 
 declare module "jest"
+
+// import Vue from "vue"
+// declare module "vue/types/vue" {
+//   interface VueConstructor {
+//     metaInfoCore: any
+//     $factorSettings: any
+//   }
+//   interface Vue {
+//     metaInfoCore: Function
+//     $factorSettings: any
+//   }
+// }
