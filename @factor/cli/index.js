9f3166ef24f6da4c6f861ffa052968c23a0ae29b
Andrew Powers <andrew@fiction.com>
2020-01-04 10:37:10 -0700

    fix(cli): windows compatibility. Closes #108. Thank you @jozsefsallai

--- @factor/cli/index.js
+++ @factor/cli/index.js
@@ -1,5 +1,12 @@
-#!/bin/sh
-":" //# comment; exec /usr/bin/env node "$0" "$@"
+#!/usr/bin/env node
+
+/**
+ * @remarks
+ * This file loads the Typescript transpiler which uses 'ts-node'
+ * and requires the main CLI script.
+ *
+ * The shebang (#!) above runs the node executable. Note that this needs to be cross-env compatible if changed (i.e. windows)
+ */
 
 require("./transpile")()
 module.exports = require("./cli")
