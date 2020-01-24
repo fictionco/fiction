834aecfd600d61c36ade24b114c40b6731b7f1a0
Andrew Powers <andrew@fiction.com>
2020-01-24 15:39:21 -0700

    feat(gcp): storage plugin

---
+++ @plugins/plugin-storage-google-cloud/README.md
@@ -0,0 +1,27 @@
+# Overview
+
+This plugin configures Factor to use Google Cloud Storage for Factor image storage.
+
+## Installation
+
+```bash
+yarn add @factor/plugin-storage-google-cloud
+```
+
+## Options and Settings
+
+### Required Configuration
+
+```git
+# .env / private keys and info
+```
+
+> After installation, run `yarn factor setup` for an easy to way to configure this plugin
+
+## How It Works
+
+This plugin uses Factor's filter system to automatically install and configure itself. If you have properly setup your API keys and other required information, it should work as intended.
+
+## Factor Setup CLI
+
+Run `yarn factor setup` for a question based CLI to help you configure this plugin's options.
---
+++ @plugins/plugin-storage-google-cloud/index.ts
@@ -0,0 +1,59 @@
+import { addFilter, addCallback } from "@factor/api"
+
+import { PostAttachment } from "@factor/attachment"
+
+/**
+ * Gets the service handler
+ */
+const getService = (): {} => {
+  return {}
+}
+
+export const setup = (): void => {
+  const credentials: string[] = []
+  if (credentials.some(_ => typeof _ === "undefined")) {
+    addFilter({
+      key: "googleCloudStorageSetup",
+      hook: "setup-needed",
+      callback: (__: { title: string }[]) => {
+        return [
+          ...__,
+          {
+            title: "Plugin: Google Cloud Storage Credentials"
+          }
+        ]
+      }
+    })
+
+    return
+  }
+
+  addFilter({
+    key: "handleUrlGCS",
+    hook: "storage-attachment-url",
+    callback: ({ buffer, key }: { buffer: Buffer; key: string }) => {
+      // const { bucket, S3 } = getService()
+      // return new Promise((resolve, reject) => {
+      //   const params = { Bucket: bucket, Key: key, Body: buffer, ACL: "public-read" }
+      //   S3.upload(params, (error: Error, data: { Location: string }) => {
+      //     if (error) reject(error)
+      //     const { Location } = data || {}
+      //     resolve(Location)
+      //   })
+      // })
+    }
+  })
+
+  addCallback({
+    key: "deleteImageS3",
+    hook: "delete-attachment",
+    callback: async (doc: PostAttachment) => {
+      // const { bucket, S3 } = getService()
+      // const key = doc.url.split("amazonaws.com/")[1]
+      // const params = { Bucket: bucket, Key: key }
+      // return await S3.deleteObject(params).promise()
+    }
+  })
+}
+
+setup()
