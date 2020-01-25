adf715db5ea72599e97b8f1a50734e577fab4372
Andrew Powers <andrew@fiction.com>
2020-01-24 17:50:37 -0700

    feat(GCS): additional, delete, readme

--- @plugins/plugin-storage-google-cloud/index.ts
+++ @plugins/plugin-storage-google-cloud/index.ts
@@ -13,10 +13,13 @@ import { PostAttachment } from "@factor/attachment"
  * process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
  * which can be set to the raw JSON string
  */
-const getService = (): { storage: Storage | undefined } => {
+const getService = (): {
+  storage: Storage | undefined;
+  bucketName: string | undefined;
+} => {
   const rawCredentialPath = process.env.GOOGLE_APPLICATION_CREDENTIALS
   const rawCredentialJSON = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
-
+  const bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET
   let storage
   let credentials
 
@@ -30,15 +33,24 @@ const getService = (): { storage: Storage | undefined } => {
     storage = new Storage({ credentials })
   }
 
-  return { storage }
+  return { storage, bucketName }
 }
 
 export const setup = (): void => {
-  const credentials: (string | undefined)[] = [
-    process.env.GOOGLE_CLOUD_STORAGE_BUCKET,
-    process.env.GOOGLE_APPLICATION_CREDENTIALS
-  ]
-  if (credentials.some(_ => typeof _ === "undefined")) {
+  /**
+   * Do we have the needed info?
+   */
+  let setupNeeded
+  if (
+    process.env.GOOGLE_CLOUD_STORAGE_BUCKET ||
+    (!process.env.GOOGLE_APPLICATION_CREDENTIALS &&
+      !process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)
+  ) {
+    setupNeeded = true
+  } else {
+    setupNeeded = false
+  }
+  if (setupNeeded) {
     addFilter({
       key: "googleCloudStorageSetup",
       hook: "setup-needed",
@@ -55,36 +67,67 @@ export const setup = (): void => {
     return
   }
 
+  /**
+   * Hooks into Factor's upload attachment filter and returns the GCS URL
+   */
   addFilter({
     key: "handleUrlGCS",
     hook: "storage-attachment-url",
     callback: async ({ buffer, key }: { buffer: Buffer; key: string }) => {
-      const { storage } = getService()
-
-      const bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET
+      const { storage, bucketName } = getService()
 
       if (!storage || !bucketName) return
 
       const bucket = storage.bucket(bucketName)
 
-      // https://googleapis.dev/nodejs/storage/latest/File.html#save
+      /**
+       * https://googleapis.dev/nodejs/storage/latest/File.html#save
+       *
+       * Save is a wrapper of
+       * https://googleapis.dev/nodejs/storage/latest/File.html#createWriteStream
+       */
       const file = bucket.file(key)
 
-      await new Promise(resolve => {
-        file.save(buffer, () => resolve())
-      })
+      await file.save(buffer)
 
-      // https://stackoverflow.com/a/20479113/1858322
+      /**
+       * Apparently Google wants us to construct the URL manually
+       * https://stackoverflow.com/a/20479113/1858322
+       */
       const url = `http://storage.googleapis.com/${bucketName}/${key}`
 
       return url
     }
   })
 
+  /**
+   * When a user deletes an image, we have the post doc
+   * Get the url, calculate the asset 'key' or file and delete with GCS
+   */
   addCallback({
     key: "deleteImageS3",
     hook: "delete-attachment",
-    callback: async (doc: PostAttachment) => {}
+    callback: async (doc: PostAttachment) => {
+      const { storage, bucketName } = getService()
+
+      if (!storage || !bucketName) return
+
+      const bucket = storage.bucket(bucketName)
+
+      const key = doc.url.split(`${bucketName}/`)[1]
+
+      const file = bucket.file(key)
+
+      /**
+       * Delete in bucket
+       * https://googleapis.dev/nodejs/storage/latest/File.html#delete
+       */
+      await file.delete()
+
+      // const apiResponse = data[0]
+
+      return
+    }
   })
 }
 
