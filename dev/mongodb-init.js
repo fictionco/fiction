6765204b0555a4dd992d98c840e6211145a32486
Justin Keller <justin@elasticbyte.net>
2020-02-20 18:03:45 -0600

    feat(docker-compose): Docker and docker-compose support (#140)

---
+++ dev/mongodb-init.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+db = db.getSiblingDB('factor');
+
+db.createUser({
+  user: 'factor',
+  pwd: 'factorDEV',
+  roles: [{
+    role: 'readWrite',
+    db: 'factor',
+  }],
+});
