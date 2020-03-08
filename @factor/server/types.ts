59fdfafdc795b4ad6f68d2d164a93cb4e80781be
Andrew Powers <andrew@fiction.com>
2020-03-07 18:25:46 -0700

    chore: pkg

--- @factor/server/types.ts
+++ @factor/server/types.ts
@@ -1,15 +1,7 @@
-import fs from "fs"
-
 export interface RendererComponents {
   bundle: string;
   template: string;
   clientManifest: object;
-}
-
-declare module "webpack-dev-middleware" {
-  interface Options {
-    fs?: typeof fs;
-  }
 }
 
 declare module "fs" {
