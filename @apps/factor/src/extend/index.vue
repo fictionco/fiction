67b9835f95fef55ef2f4abd0a8db12a090eb61ad
finestpixels <ray@fiction.com>
2020-03-18 16:28:16 -0700

    feat(extend): extensions ui

--- @apps/factor/src/extend/index.vue
+++ @apps/factor/src/extend/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="plugins-container">
-    <div class="plugin-index-head">
+  <div class="extend-container">
+    <div class="extend-index-head">
       <div class="content-pad">
         <template v-if="extensionType == 'plugin'">
           <h1 class="title">Factor Plugins</h1>
@@ -26,67 +26,23 @@
       </div>
     </div>
 
-    <div v-else class="plugins-wrap content-pad">
-      <div class="content">
-        <section v-if="extensionFeatured.length > 0" class="plugins-featured">
-          <header class="section-header">
-            <h1 class="title">Featured</h1>
-          </header>
-          <div class="plugins-grid">
-            <factor-link
-              v-for="(item, index) in extensionFeatured"
-              :key="index"
-              :path="extensionPermalink({ name: item._id })"
-              class="entry-plugin"
-            >
-              <div class="entry-content">
-                <div class="entry-image">
-                  <img :src="extensionIcon(item)" :alt="`${item.name} Icon`" />
-                </div>
-                <h3 class="title">{{ titleFromPackage(item) }}</h3>
-                <div class="meta">
-                  <div class="authors">by {{ getAuthors(item) }}</div>
-                </div>
-
-                <p v-if="item.description" class="text">{{ item.description }}</p>
-              </div>
-              <div class="entry-footer">
-                <div
-                  v-if="item.downloads"
-                  class="downloads"
-                >&darr; {{ formatDownloads(item.downloads) }} downloads</div>
-              </div>
-            </factor-link>
-          </div>
-        </section>
-
-        <section class="plugins-all">
-          <header class="section-header">
-            <h1 class="title">All</h1>
-          </header>
-          <factor-link
-            v-for="(item, i) in extensionIndex"
-            :key="i"
-            :path="extensionPermalink({ name: item._id })"
-            class="entry-plugin"
-          >
-            <div class="entry-image">
-              <img :src="extensionIcon(item)" :alt="item.name" />
-            </div>
-
-            <div class="entry-content">
-              <h3 class="title">{{ titleFromPackage(item) }}</h3>
-              <div class="meta">
-                <div class="authors">by {{ getAuthors(item) }}</div>
-              </div>
-
-              <p v-if="item.description" class="text">{{ item.description }}</p>
-            </div>
-          </factor-link>
-        </section>
+    <div v-else-if="extensionType == 'plugin'" class="extensions-wrap plugins-wrap content-pad">
+      {{ getData }}
+      <div class="plugins-grid">
+        <grid-item-plugin />
+        <grid-item-plugin />
+        <grid-item-plugin />
       </div>
       <div>
         <widget-sidebar :index-data="extensionIndex" />
+      </div>
+    </div>
+
+    <div v-else class="extensions-wrap themes-wrap content-pad">
+      <div class="themes-grid">
+        <grid-item-theme />
+        <grid-item-theme />
+        <grid-item-theme />
       </div>
     </div>
 
@@ -110,6 +66,8 @@ export default Vue.extend({
   components: {
     "widget-sidebar": () => import("./sidebar.vue"),
     "widget-cta": () => import("./el/cta.vue"),
+    "grid-item-plugin": () => import("./grid-item-plugin.vue"),
+    "grid-item-theme": () => import("./grid-item-theme.vue"),
     factorLoadingRing,
     factorLink
   },
@@ -154,19 +112,19 @@ export default Vue.extend({
   },
   metaInfo() {
     return {
-      title: "Factor Plugins",
+      title: "Factor " + this.extensionType,
       description: "Add advanced features to your app in seconds."
     }
   }
 })
 </script>
 <style lang="less">
-.plugins-container {
+.extend-container {
   @import "~./style.less";
   padding-top: 45px;
   font-weight: 400;
   overflow: hidden;
-  .plugin-index-head {
+  .extend-index-head {
     padding: 6em 0;
     text-align: center;
     .title {
@@ -235,17 +193,22 @@ export default Vue.extend({
   }
 
   /* PAGE CONTENT */
-  .plugins-wrap {
-    display: grid;
-    grid-template-columns: 7fr 3fr;
-    grid-gap: 6rem;
-    padding-top: 2rem;
+  .extensions-wrap {
+    &.plugins-wrap {
+      display: grid;
+      grid-gap: 4rem;
+      grid-template-columns: 7fr 3fr;
+
+      @media (max-width: 900px) {
+        grid-template-columns: 1fr;
+        grid-gap: 2rem;
+      }
+    }
 
     .section-header {
       .title {
-        font-size: 1.6em;
-        font-weight: 500;
-        line-height: 1.1;
+        font-size: 1.4em;
+        font-weight: var(--font-weight-bold, 700);
         letter-spacing: -0.02em;
 
         @media (max-width: 900px) {
@@ -255,178 +218,14 @@ export default Vue.extend({
       }
     }
 
-    @media (max-width: 900px) {
-      grid-template-columns: 1fr;
-      grid-gap: 1rem;
-    }
-  }
-  .entry-plugin {
-    box-shadow: 0 1px 3px #afb6ca;
-    color: var(--color-text);
-    border-radius: 6px;
-    background: #fff;
-    transition: 0.3s all;
-    &:hover {
-      transform: translateY(-1px);
-      box-shadow: 0 1px 3px 0px #afb6ca;
-      background: #fafafa;
-    }
-  }
-
-  //  FEATURED PLUGINS
-  .plugins-featured {
-    .section-header {
-      margin: 0 0 1rem;
-    }
-    .plugins-grid {
+    .plugins-grid,
+    .themes-grid {
       display: grid;
       grid-template-columns: 1fr 1fr;
-      grid-gap: 1.5rem;
+      grid-gap: 3rem;
+
       @media (max-width: 900px) {
         grid-template-columns: 1fr;
-      }
-    }
-    .entry-plugin {
-      padding: 1.5rem;
-
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-
-      .entry-image {
-        height: 70px;
-        width: 70px;
-        margin-bottom: 1rem;
-        border-radius: 50%;
-        overflow: hidden;
-        background: var(--color-bg-contrast);
-        border: 1px solid var(--color-bg-contrast-more);
-        box-shadow: 0 1px 3px -1px rgba(0, 0, 0, 0.3);
-        img {
-          width: 100%;
-        }
-      }
-      .entry-content {
-        overflow: hidden;
-        margin: 1em 0;
-        .title {
-          font-size: 1.6em;
-          line-height: 1.2em;
-          margin-bottom: 5px;
-          text-transform: capitalize;
-        }
-        .meta {
-          color: rgba(var(--color-text-rgb), 0.6);
-          > div {
-            display: inline-block;
-            margin-right: 1rem;
-            &:last-child {
-              margin-right: 0;
-            }
-            &.categories,
-            &.authors {
-              display: inline-block;
-              .category,
-              .author {
-                display: inherit;
-                &:after {
-                  content: ", ";
-                  padding-right: 5px;
-                }
-                &:last-of-type {
-                  &:after {
-                    content: initial;
-                  }
-                }
-              }
-            }
-          }
-        }
-        .text {
-          line-height: 1.7em;
-          margin: 0.5em 0;
-        }
-      }
-    }
-  }
-
-  // // SEARCH AND CATEGORIES
-  // .plugins-search-wrap {
-  //   display: grid;
-  //   grid-gap: 1rem;
-  //   grid-template-columns: 1fr auto;
-  // }
-
-  //  ENTRIES ALL
-  .plugins-all {
-    .section-header {
-      margin: 2rem 0 1.5rem;
-    }
-    .entry-plugin {
-      display: grid;
-      grid-template-columns: 70px 3fr;
-      grid-gap: 2rem;
-      align-items: flex-start;
-      margin-bottom: 1.5rem;
-      padding: 2rem;
-      background: #fff;
-
-      @media (max-width: 900px) {
-        display: block;
-      }
-
-      .entry-image {
-        height: 70px;
-        width: 70px;
-        margin-bottom: 1rem;
-        border-radius: 50%;
-        overflow: hidden;
-        background: var(--color-bg-contrast);
-        border: 1px solid var(--color-bg-contrast-more);
-        box-shadow: 0 1px 3px -1px rgba(0, 0, 0, 0.3);
-        img {
-          width: 100%;
-        }
-      }
-      .entry-content {
-        overflow: hidden;
-        .title {
-          font-size: 1.6em;
-          line-height: 1.2em;
-          margin-bottom: 5px;
-          text-transform: capitalize;
-        }
-        .meta {
-          color: rgba(var(--color-text-rgb), 0.6);
-          > div {
-            display: inline-block;
-            margin-right: 1rem;
-            &:last-child {
-              margin-right: 0;
-            }
-            &.categories,
-            &.authors {
-              display: inline-block;
-              .category,
-              .author {
-                display: inherit;
-                &:after {
-                  content: ", ";
-                  padding-right: 5px;
-                }
-                &:last-of-type {
-                  &:after {
-                    content: initial;
-                  }
-                }
-              }
-            }
-          }
-        }
-        .text {
-          line-height: 1.7em;
-          margin: 0.5em 0;
-        }
       }
     }
   }
