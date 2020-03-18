009a676d1545ed76e10cd7150d86f3d826bd6a33
Andrew Powers <andrew@fiction.com>
2020-03-18 16:59:25 -0600

    feat(setup): finish ui

--- @factor/loader/app/setup.vue
+++ @factor/loader/app/setup.vue
@@ -9,9 +9,11 @@
         :class="[step == index + 1 ? 'active' : '', isComplete(page._id) ? 'complete' : '']"
       >
         <span v-if="isComplete(page._id)" class="check">
-          <img src="./img/check.svg" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
+          </svg>
         </span>
-        <span v-else class="num">{{ index + 1 }}</span>
+        <span class="num">{{ page._id }}</span>
       </router-link>
     </div>
     <div class="setup-content">
@@ -29,7 +31,7 @@
                 </div>
               </div>
               <div class="actions">
-                <router-link class="btn" to="/setup?step=2">Create Your Account &rarr;</router-link>
+                <div class="btn primary next-step" @click="nextStep()">Start &rarr;</div>
               </div>
             </div>
           </transition>
@@ -37,11 +39,13 @@
         <div v-else-if="isStep('app')" key="account" class="page-item account">
           <transition appear>
             <div class="page-item-pad">
-              <div class="user-image">
-                <div class="user-image-wrap">
-                  <img :src="appAvatar" />
+              <transition appear>
+                <div v-if="form.appEmail" class="user-image">
+                  <a class="user-image-wrap" target="_blank" href="https://gravatar.com">
+                    <img :src="appAvatar" title="gravatar" alt="gravatar" />
+                  </a>
                 </div>
-              </div>
+              </transition>
               <div class="header">
                 <div class="title">Your App</div>
                 <div class="sub-title">You can change this later.</div>
@@ -56,7 +60,15 @@
               </form>
 
               <div class="actions">
-                <router-link class="btn" to="/setup?step=3">Next Step &rarr;</router-link>
+                <div
+                  class="btn next-step"
+                  :class="isComplete('app') ? 'primary': 'default'"
+                  @click="nextStep()"
+                >Next Step &rarr;</div>
+                <div class="skipper">
+                  or
+                  <span class="skip-step" @click="skip.app = true;nextStep()">skip this step</span>
+                </div>
               </div>
             </div>
           </transition>
@@ -64,26 +76,36 @@
         <div v-else-if="isStep('account')" key="account" class="page-item account">
           <transition appear>
             <div class="page-item-pad">
-              <div class="user-image">
-                <div class="user-image-wrap">
-                  <img :src="userAvatar" />
+              <transition appear>
+                <div v-if="form.email" class="user-image">
+                  <a class="user-image-wrap" target="_blank" href="https://gravatar.com">
+                    <img :src="userAvatar" title="gravatar" alt="gravatar" />
+                  </a>
                 </div>
-              </div>
+              </transition>
               <div class="header">
                 <div class="title">Your Account</div>
                 <div class="sub-title">Create an Admin User</div>
               </div>
               <form class="setup-form">
-                <div v-for="(input, index) in inputs.account" :key="index" class="input-item">
-                  <transition appear>
-                    <label v-if="form[input._id]">{{ input.label }}</label>
-                  </transition>
-                  <input v-model="form[input._id]" :type="input.type" :placeholder="input.label" />
-                </div>
+                <form-input
+                  v-for="(input, index) in inputs.account"
+                  :key="index"
+                  v-model="form[input._id]"
+                  :input="input"
+                />
               </form>
 
               <div class="actions">
-                <router-link class="btn" to="/setup?step=3">Next Step &rarr;</router-link>
+                <div
+                  class="btn next-step"
+                  :class="isComplete('account') ? 'primary': 'default'"
+                  @click="nextStep()"
+                >Next Step &rarr;</div>
+                <div class="skipper">
+                  or
+                  <span class="skip-step" @click="skip.account = true;nextStep()">skip this step</span>
+                </div>
               </div>
             </div>
           </transition>
@@ -93,21 +115,24 @@
             <div class="gallery-item-pad page-item-pad">
               <div class="header">
                 <div class="title">Select Default Theme</div>
-                <div class="sub-title">You can always change this later.</div>
+                <div class="sub-title">You can change this later.</div>
               </div>
               <div class="gallery-item-action">
                 <form class="setup-form">
-                  <div v-for="(input, index) in inputs.theme" :key="index" class="input-item">
-                    <transition appear>
-                      <label v-if="form[input._id]">{{ input.label }}</label>
-                    </transition>
-                    <input v-model="form[input._id]" :type="input.type" :placeholder="input.label" />
-                  </div>
+                  <form-input
+                    v-for="(input, index) in inputs.theme"
+                    :key="index"
+                    v-model="form[input._id]"
+                    :input="input"
+                  />
                   <div class="actions">
-                    <router-link class="btn" to="/setup?step=4">Select Theme &rarr;</router-link>
+                    <div class="btn primary primary next-step" @click="nextStep()">Next Step &rarr;</div>
                     <div class="skipper">
                       or
-                      <router-link class="skip" to="/setup?step=4">skip this step</router-link>
+                      <span
+                        class="skip-step"
+                        @click="form.theme = ''; skip.theme = true;nextStep()"
+                      >don't use a theme</span>
                     </div>
                   </div>
                 </form>
@@ -128,6 +153,57 @@
             </div>
           </transition>
         </div>
+        <div v-else-if="isStep('db')" key="db" class="page-item db">
+          <transition appear>
+            <div class="page-item-pad">
+              <div class="setup-image">
+                <img src="./img/mongodb.svg" alt="Mongo" />
+              </div>
+              <div class="header">
+                <div class="title">Database Connection</div>
+                <div class="sub-title">
+                  A MongoDB URL.
+                  <br />Handles all dynamic data.
+                  <br />
+                  <a
+                    class="addendum"
+                    href="https://docs.mongodb.com/manual/reference/connection-string/"
+                    target="_blank"
+                  >MongoDB Docs</a>
+                  <a
+                    class="addendum"
+                    href="https://youtu.be/KKyag6t98g8?t=90"
+                    target="_blank"
+                  >Video Tutorial</a>
+                </div>
+              </div>
+              <form class="setup-form">
+                <form-input
+                  v-for="(input, index) in inputs.db"
+                  :key="index"
+                  v-model="form[input._id]"
+                  :input="input"
+                />
+              </form>
+
+              <div class="actions">
+                <div
+                  class="btn next-step"
+                  :class="isComplete('db') ? 'primary': 'default'"
+                  @click="nextStep()"
+                >Next Step &rarr;</div>
+
+                <div class="skipper">
+                  or
+                  <span
+                    class="skip-step"
+                    @click="skip.app = true;nextStep()"
+                  >skip and use the demo db</span>
+                </div>
+              </div>
+            </div>
+          </transition>
+        </div>
         <div v-else-if="isStep('done')" key="done" class="page-item">
           <transition appear>
             <div class="page-item-pad">
@@ -141,7 +217,7 @@
                 >You can now customize your app and modify other settings on your dashboard.</div>
               </div>
               <div class="actions add-space">
-                <div class="btn" @click="sendData()">Build App &rarr;</div>
+                <div class="btn primary" @click="sendData()">Save Config &amp; Build App &rarr;</div>
               </div>
             </div>
           </transition>
@@ -172,6 +248,7 @@ export default Vue.extend({
   mixins: [capitalizeMixin, logMixin, sseMixin, storageMixin],
   data() {
     return {
+      skip: {},
       baseURL: window.$BASE_URL,
       form: {},
       themes: [
@@ -195,18 +272,22 @@ export default Vue.extend({
         app: [
           {
             type: "text",
-            label: "Application Title",
+            label: "App Name",
+            placeholder: "Example App",
             _id: "appName"
           },
           {
             type: "url",
-            label: "Production URL",
+            label: "URL in Production",
+            placeholder: "https://www.example.com",
+            description: "Needed for transactional email",
             _id: "appUrl"
           },
           {
             type: "email",
-            label: "Application Email",
-            description: "For transactional email",
+            label: `App Email Address`,
+            placeholder: "team@example.com",
+            description: "Needed for transactional email",
             _id: "appEmail"
           }
         ],
@@ -214,26 +295,38 @@ export default Vue.extend({
           {
             type: "text",
             label: "Full Name",
+            placeholder: "",
             _id: "displayName"
           },
           {
             type: "email",
             label: "Email",
+            placeholder: "email@example.com",
             _id: "email"
           },
           {
             type: "password",
             label: "Password",
+            placeholder: "",
             _id: "password"
           }
         ],
-        theme: [{ type: "text", label: "Theme Package", _id: "theme" }]
+        theme: [{ type: "text", label: "Theme Package", _id: "theme" }],
+        db: [
+          {
+            type: "url",
+            label: "Db Connection",
+            _id: "db",
+            placeholder:
+              "mongodb+srv://demo:demo@cluster0-yxsfy.mongodb.net/demo?retryWrites=true&w=majority"
+          }
+        ]
       },
       steps: [
         {
           _id: "welcome",
           complete: () => {
-            return this.step > 1 ? true : false
+            return this.step >= 1 ? true : false
           }
         },
         {
@@ -251,13 +344,19 @@ export default Vue.extend({
         {
           _id: "theme",
           complete: () => {
-            return this.form.theme ? true : false
+            return this.step > this.getStepIndex("theme") ? true : false
+          }
+        },
+        {
+          _id: "db",
+          complete: () => {
+            return this.step > this.getStepIndex("db") ? true : false
           }
         },
         {
           _id: "done",
           complete: () => {
-            return this.isComplete("account") && this.isComplete("theme") ? true : false
+            return this.isStep("done") ? true : false
           }
         }
       ],
@@ -272,16 +371,22 @@ export default Vue.extend({
       return gravatar.url(this.form.email, { s: "200", d: "retro" }) || ""
     },
     appAvatar() {
-      return gravatar.url(this.form.appEmail, { s: "200", d: "retro" }) || ""
+      return gravatar.url(this.form.appEmail, { s: "200", d: "identicon" }) || ""
     }
   },
   methods: {
+    nextStep() {
+      this.$router.push({ path: "/setup", query: { step: Number(this.step) + 1 } })
+    },
     getStep(_id) {
+      return this.steps.find(_ => _id == _._id)
+    },
+    getStepIndex(_id) {
       const index = this.steps.findIndex(_ => _id == _._id)
       return index + 1
     },
     isStep(_id) {
-      return this.getStep(_id) == this.step ? true : false
+      return this.getStepIndex(_id) == this.step ? true : false
     },
     setTheme(theme) {
       this.$set(this.form, "theme", theme)
@@ -326,38 +431,47 @@ export default Vue.extend({
   .pages {
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
     padding: 1rem;
     .pg {
       user-select: none;
       text-decoration: none;
-      width: 2.5rem;
-      height: 2.5rem;
-      line-height: 2.5rem;
+      line-height: 1;
+      padding: 0.23rem 1rem 0.2rem;
+
+      display: flex;
+      align-items: center;
       .check {
-        line-height: 2.5;
-        img {
+        svg {
           width: 1.1em;
+          path {
+            fill: var(--color-primary);
+          }
         }
+        margin-right: 0.5rem;
       }
       text-align: center;
       border-radius: 8px;
-
-      color: var(--color-text-subtle);
+      background: #fff;
+      color: var(--color-text);
       border: 1px solid var(--color-text-subtle);
       font-weight: 700;
-      font-size: 1.2em;
+      text-transform: uppercase;
+      font-size: 0.8em;
+      .num {
+        line-height: 1.8;
+      }
       margin: 0.5rem;
 
       transition: opacity 0.2s;
       &.active,
       &:hover {
-        background: var(--color-text-subtle);
-        opacity: 1;
         color: #fff;
-      }
-      &.complete {
-        border: 1px solid var(--color-text-subtle);
-        background: var(--color-text-subtle);
+        border: 1px solid var(--color-primary);
+        background: var(--color-primary);
+        .check svg path {
+          fill: #fff;
+        }
       }
     }
   }
@@ -394,12 +508,19 @@ export default Vue.extend({
       }
     }
     .page-item-pad {
-      padding: 3rem;
+      padding: 3rem 3rem 5rem;
       border-radius: 10px;
       background: #fff;
       box-shadow: var(--panel-shadow);
+      @media (max-width: 900px) {
+        padding: 1rem 1rem 3rem;
+      }
       .setup-image {
         text-align: center;
+        img {
+          min-width: 50px;
+        }
+        margin-bottom: 2rem;
       }
       .user-image {
         text-align: center;
@@ -423,39 +544,41 @@ export default Vue.extend({
   .gallery-item {
   }
 
-  .page-item {
-    width: 650px;
-    max-width: 95vw;
-    margin: 0 1rem;
-    .page-item-pad {
-      padding: 3rem;
-      border-radius: 10px;
-      background: #fff;
-      box-shadow: var(--panel-shadow);
-      .setup-image {
-        text-align: center;
-      }
-      .user-image {
-        text-align: center;
+  // .page-item {
+  //   width: 650px;
+  //   max-width: 95vw;
+  //   margin: 0 1rem;
+  //   .page-item-pad {
+  //     padding: 3rem;
+  //     border-radius: 10px;
+  //     background: #fff;
+  //     box-shadow: var(--panel-shadow);
+  //     .setup-image {
+  //       text-align: center;
+  //     }
+  //     .user-image {
+  //       text-align: center;
 
-        margin-bottom: 1rem;
-        img {
-          width: 80px;
-          background: #fff;
-          border-radius: 50%;
-          display: inline-block;
-        }
-      }
-    }
-    &.account {
-      .header {
-        margin-bottom: 0;
-      }
-    }
-  }
+  //       margin-bottom: 1rem;
+  //       img {
+  //         width: 80px;
+  //         background: #fff;
+  //         border-radius: 50%;
+  //         display: inline-block;
+  //       }
+  //     }
+  //   }
+  //   &.account {
+  //     .header {
+  //       margin-bottom: 0;
+  //     }
+  //   }
+  // }
   .header {
     text-align: center;
-
+    a {
+      color: inherit;
+    }
     .title {
       letter-spacing: -0.02em;
       font-weight: 700;
@@ -466,6 +589,12 @@ export default Vue.extend({
       opacity: 0.7;
       font-size: 1.35em;
       line-height: 1.6;
+    }
+    .addendum {
+      font-size: 0.8em;
+      margin: 0 0.5rem;
+      color: var(--color-primary);
+      text-decoration: none;
     }
   }
 
@@ -491,12 +620,18 @@ export default Vue.extend({
     &.add-space {
       margin-top: 2rem;
     }
+    .back-step,
+    .next-step,
+    .skip-step {
+      cursor: pointer;
+    }
+
     .skipper {
       display: block;
       margin-top: 1rem;
       opacity: 0.4;
-      .skip {
-        color: inherit;
+      .skip-step {
+        text-decoration: underline;
       }
     }
   }
