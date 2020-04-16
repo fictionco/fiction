c526463908c7fb6882e3c4c009e8a07b27fef1aa
Andrew Powers <andrew@fiction.com>
2020-04-15 23:25:13 -0700

    feat(bugsnag): upgrade to latest

--- @plugins/plugin-bugsnag/index.ts
+++ @plugins/plugin-bugsnag/index.ts
@@ -1,45 +1,27 @@
-import bugsnag, { Bugsnag } from "@bugsnag/js"
+import bugsnag from "@bugsnag/js"
 
-import bugsnagVue from "@bugsnag/plugin-vue"
-import { log, onEvent, addCallback, setting } from "@factor/api"
+import BugsnagPluginVue from "@bugsnag/plugin-vue"
+import { setting } from "@factor/api"
 
-import { userInitialized } from "@factor/user"
-import Vue from "vue"
+import { currentUser } from "@factor/user"
 
 const clientApiKey = setting("bugsnag.clientApiKey")
-
-export const customLogger = (): Bugsnag.ILogger => {
-  return {
-    debug: (): void => {},
-    info: (): void => {},
-    warn: (): Function => log.warn,
-    error: (): Function => log.error,
-  }
-}
 
 export const setup = (): void => {
   if (!clientApiKey || process.env.NODE_ENV == "development") return
 
   const appVersion = setting("package.version") || "0.0.0"
 
-  const bugsnagClient = bugsnag({
+  bugsnag.start({
     apiKey: clientApiKey,
-    logger: customLogger(),
-    appVersion,
-  })
-
-  bugsnagClient.use(bugsnagVue, Vue)
-
-  addCallback({
-    key: "bugsnag",
-    hook: "initialize-app",
-    callback: async (): Promise<void> => {
-      onEvent("error", (error: Error) => bugsnagClient.notify(error))
-
-      const user = await userInitialized()
-
-      if (user) bugsnagClient.user = user
+    plugins: [new BugsnagPluginVue()],
+    onError: function (event) {
+      const user = currentUser()
+      if (user) {
+        event.setUser(user._id, user.email, user.displayName)
+      }
     },
+    appVersion,
   })
 }
 
