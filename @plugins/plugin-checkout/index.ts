fa48d8578a1065b2e96a8377888d25618e205e3b
Andrew Powers <andrew@fiction.com>
2020-05-06 11:53:47 -0700

    fix: don't show in dev

--- @plugins/plugin-checkout/index.ts
+++ @plugins/plugin-checkout/index.ts
@@ -9,26 +9,28 @@ addRoutes({
   routes: [setting("checkout.routes.checkout")],
 })
 
-addDashboardMenu({
-  name: "Subscription",
-  path: "/subscription",
-  key: "subscription",
-  component: (): Promise<any> => import("./dashboard/wrap.vue"),
-  children: [
-    {
-      name: "Subscription",
-      path: "/",
-      component: (): Promise<any> => import("./dashboard/subscription.vue"),
-    },
-    {
-      name: "Payment Method",
-      path: "/payment-method",
-      component: (): Promise<any> => import("./dashboard/plans.vue"),
-    },
-    {
-      name: "Invoices",
-      path: "/invoices",
-      component: (): Promise<any> => import("./dashboard/plans.vue"),
-    },
-  ],
-})
+if (process.env.NODE_ENV == "development") {
+  addDashboardMenu({
+    name: "Subscription",
+    path: "/subscription",
+    key: "subscription",
+    component: (): Promise<any> => import("./dashboard/wrap.vue"),
+    children: [
+      {
+        name: "Subscription",
+        path: "/",
+        component: (): Promise<any> => import("./dashboard/subscription.vue"),
+      },
+      {
+        name: "Payment Method",
+        path: "/payment-method",
+        component: (): Promise<any> => import("./dashboard/plans.vue"),
+      },
+      {
+        name: "Invoices",
+        path: "/invoices",
+        component: (): Promise<any> => import("./dashboard/plans.vue"),
+      },
+    ],
+  })
+}
