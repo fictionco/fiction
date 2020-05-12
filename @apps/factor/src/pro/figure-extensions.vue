59eb149e37f50c5dc186175c0384da0e559444ee
finestpixels <ray@fiction.com>
2020-05-12 13:54:07 -0700

    feat(pro): page setup

---
+++ @apps/factor/src/pro/figure-extensions.vue
@@ -0,0 +1,90 @@
+<template>
+  <figure ref="wrapper" class="figure-extensions">
+    <div class="stage-wrap" :style="{ transform: `scale(${scale})` }">
+      <div class="extensions">
+        <div class="screenshot card-extension-list">
+          <img src="./img/card-extensions-list.svg" alt="Extensions List" />
+        </div>
+        <div class="screenshot card-extension-page">
+          <img src="./img/card-extensions-page.svg" alt="Extensions Page" />
+        </div>
+      </div>
+    </div>
+  </figure>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      width: 500,
+    }
+  },
+  computed: {
+    scale(this: any) {
+      return Math.max(Math.min(this.width / 500, 1), 0.5)
+    },
+  },
+  mounted() {
+    this.width = this.getWidth()
+
+    window.addEventListener("resize", () => {
+      this.width = this.getWidth()
+    })
+  },
+  methods: {
+    getWidth(this: any) {
+      return this.$refs.wrapper ? this.$refs.wrapper.clientWidth : 100
+    },
+  },
+}
+</script>
+
+<style lang="less">
+figure.figure-extensions {
+  max-width: 100%;
+  @media (max-width: 900px) {
+    margin: 1rem auto 4rem;
+    width: 450px;
+  }
+
+  .stage-wrap {
+    transform-origin: center right;
+  }
+  .extensions {
+    padding: 34% 0;
+    width: 500px;
+    position: relative;
+    transform: scale(1);
+    transform-style: preserve-3d;
+    @media (max-width: 900px) {
+      transform: scale(1) translate(-0, -0px);
+    }
+    .screenshot {
+      background: #fff;
+      display: inline-block;
+      &.card-extension-list {
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: 0;
+        transform: translateY(-45px);
+        box-shadow: 0px 0px 3px rgba(50, 50, 93, 0.2), 0px 14px 32px rgba(50, 50, 93, 0.1);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+      &.card-extension-page {
+        position: absolute;
+        top: 0;
+        left: 0;
+        transform: translateZ(-50px) translateX(-90px) scale(1) perspective(1050px);
+        background: #ffffff;
+        box-shadow: 0px 0px 3px rgba(50, 50, 93, 0.2), 0px 14px 32px rgba(50, 50, 93, 0.1);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+    }
+    perspective: 1000px;
+  }
+}
+</style>
---
+++ @apps/factor/src/pro/splash.vue
@@ -0,0 +1,144 @@
+<template>
+  <section class="splash">
+    <div class="splash-inner">
+      <div class="content">
+        <h1 class="page-title">Factor Pro Suite</h1>
+        <h3
+          class="page-title-sub"
+        >Lorem ipsum sit amet, consectetur adipiscing elit, sed do eiusmod.</h3>
+
+        <div class="actions">
+          <factor-link btn="primary" path="/plans">Pick a plan &rarr;</factor-link>
+        </div>
+      </div>
+
+      <div class="figure-container">
+        <splash-figure />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { factorLink } from "@factor/ui"
+
+export default {
+  components: {
+    factorLink,
+    splashFigure: () => import("./figure-splash.vue"),
+  },
+}
+</script>
+
+<style lang="less">
+.splash {
+  position: relative;
+
+  .splash-inner {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 4em;
+    padding: 10rem 3rem 6rem;
+    align-items: center;
+
+    @media (min-height: 1000px) {
+      padding-top: 170px;
+    }
+
+    @media (max-width: 900px) {
+      grid-template-columns: 1fr;
+      text-align: left;
+      padding: 3rem 0 6rem;
+    }
+    .figure-container {
+      min-width: 0;
+      display: grid;
+      justify-content: center;
+    }
+    .content {
+      max-width: 550px;
+      justify-self: flex-end;
+
+      h1 {
+        z-index: 5;
+        position: relative;
+      }
+
+      .page-sup {
+        text-transform: uppercase;
+        font-weight: 700;
+        color: var(--color-text-secondary);
+        margin-bottom: 1rem;
+        opacity: 0.5;
+      }
+      .page-title,
+      .page-title-sub {
+        letter-spacing: -0.035em;
+      }
+
+      .page-title {
+        font-size: 4em;
+        letter-spacing: -0.035em;
+        line-height: 1.1;
+        font-weight: 700;
+        margin-bottom: 1rem;
+      }
+
+      .page-title-sub {
+        margin-top: 2rem;
+        font-size: 2.2em;
+        font-weight: 400;
+        color: var(--color-text-secondary);
+        .highlight {
+          color: #5c84a4;
+        }
+      }
+
+      .actions {
+        font-size: 1.3em;
+        margin-top: 2rem;
+        .factor-btn {
+          margin-right: 1rem;
+          margin-bottom: 1rem;
+        }
+      }
+
+      .sub-actions {
+        margin-top: 1.5rem;
+      }
+
+      @media (max-width: 1200px) {
+        max-width: 450px;
+        .page-title {
+          font-size: 3em;
+        }
+        .page-title-sub {
+          font-size: 1.5em;
+        }
+
+        .actions {
+          font-size: 1.1em;
+        }
+      }
+
+      @media (max-width: 900px) {
+        padding: 1em 2em;
+        justify-self: center;
+        max-width: 100%;
+        .page-title {
+          font-size: 2em;
+          line-height: 1.3;
+        }
+        .page-title-sub {
+          font-size: 1.4em;
+          line-height: 1.4;
+        }
+
+        .actions {
+          font-size: 1em;
+        }
+      }
+    }
+  }
+}
+</style>
