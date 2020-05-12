f3328eec2d19f72ec506ba2981c2401ed87a383d
finestpixels <ray@fiction.com>
2020-05-12 13:54:07 -0700

    feat(pro): page setup

---
+++ @apps/factor/src/pro/figure-splash.vue
@@ -0,0 +1,82 @@
+<template>
+  <figure ref="wrapper" class="figure-splash">
+    <div class="stage-wrap">
+      <div class="stage">
+        <div class="screenshot card-settings">
+          <img src="./img/card-settings.svg" alt="Settings" />
+        </div>
+        <div class="screenshot card-support">
+          <img src="./img/card-support.svg" alt="Support" />
+        </div>
+        <div class="screenshot card-extensions">
+          <img src="./img/card-extensions.svg" alt="Extensions" />
+        </div>
+      </div>
+    </div>
+  </figure>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style lang="less">
+figure.figure-splash {
+  max-width: 100%;
+  @media (max-width: 900px) {
+    margin: 1rem auto 4rem;
+    width: 450px;
+  }
+
+  .stage-wrap {
+    transform-origin: center left;
+  }
+  .stage {
+    padding: 30% 0;
+    width: 560px;
+    position: relative;
+    transform: scale(1.1);
+    transform-style: preserve-3d;
+    @media (max-width: 900px) {
+      transform: scale(1) translate(-0, -0px);
+    }
+    .screenshot {
+      top: 35px;
+      background: #fff;
+      display: inline-block;
+      &.card-settings {
+        position: absolute;
+        top: -30px;
+        right: 110px;
+        transform: scale(1) perspective(1050px);
+        background: #ffffff;
+        box-shadow: 0px 0px 3px rgba(50, 50, 93, 0.2), 0px 14px 32px rgba(50, 50, 93, 0.1);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+      &.card-support {
+        position: absolute;
+        top: 20px;
+        left: 70px;
+        z-index: 0;
+        transform: scale(1) translateZ(-40px);
+        box-shadow: 0px 20px 90px rgba(50, 50, 93, 0.13),
+          0 15px 35px rgba(50, 50, 93, 0.11), 0 5px 15px rgba(0, 0, 0, 0.07);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+      &.card-extensions {
+        position: absolute;
+        top: 270px;
+        left: 0;
+        transform: scale(1) perspective(1050px);
+        background: #ffffff;
+        box-shadow: 0px 0px 3px rgba(50, 50, 93, 0.2), 0px 14px 32px rgba(50, 50, 93, 0.1);
+        overflow: hidden;
+        border-radius: 4px;
+      }
+    }
+    perspective: 1000px;
+  }
+}
+</style>
---
+++ @apps/factor/src/pro/section-benefits.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="factor-pro-benefits">
+    <ul class="features-list">
+      <li class="feature-block">
+        <home-icon icon="settings" />
+        <h3 class="title">Advanced Settings</h3>
+        <p
+          class="text"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+      </li>
+
+      <li class="feature-block">
+        <home-icon icon="premium" />
+        <h3 class="title">Premium Features</h3>
+        <p
+          class="text"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+      </li>
+
+      <li class="feature-block">
+        <home-icon icon="support" />
+        <h3 class="title">Help &amp; Support</h3>
+        <p
+          class="text"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  components: {
+    homeIcon: () => import("./icon.vue"),
+  },
+}
+</script>
+<style lang="less">
+.factor-pro-benefits {
+  position: relative;
+
+  @media (max-width: 767px) {
+    padding-bottom: 80px;
+  }
+
+  .features-list {
+    font-size: 1.1em;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 2em;
+    list-style: none;
+    @media (max-width: 767px) {
+      grid-template-columns: 1fr;
+    }
+    .feature-block {
+      position: relative;
+      .title {
+        font-weight: 700;
+        font-size: 1.2em;
+        line-height: 1.5em;
+      }
+      .text {
+        font-size: 0.94em;
+        line-height: 1.7em;
+        opacity: 0.85;
+      }
+      .icon {
+        width: 48px;
+        height: 48px;
+
+        margin: 10px 0 20px 0;
+      }
+      h3,
+      p {
+        margin-bottom: 10px;
+      }
+      p:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+}
+</style>
