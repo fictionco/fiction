a1916f2f655e93fdf9ced54a1e3f697477b3e3e9
finestpixels <ray@fiction.com>
2020-05-26 10:49:31 -0700

    feat(factor): plugins page setup

--- @apps/factor/src/extend/slider-plugin.vue
+++ @apps/factor/src/extend/slider-plugin.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="grid-plugin-container">
-    <header v-if="title" class="section-header">
-      <h1 class="title">{{ title }}</h1>
-    </header>
-    <div class="plugin-grid">
+  <div class="slider-plugin-container">
+    <factor-spinner v-if="loading" />
+    <section v-else-if="extensions.length > 0" class="slider-plugin">
+      <header class="section-header">
+        <h1 class="title">{{ title }}</h1>
+      </header>
       <div
         v-for="(item, index) in extensions"
         :key="index"
-        class="grid-item-plugin"
+        class="featured-item-plugin"
+        :class="{ 'active': active === index }"
         @click="$router.push({ path: `/plugin/${encodeURIComponent(item.permalink)}` })"
       >
-        <div class="entry-media" :style="mediaStyle(item)" :class="imageStyle(item)">
-          <img :src="item.icon" :alt="`${item.title} Icon`" class="extend-icon" />
-        </div>
+        <div class="entry-media" :style="mediaStyle(item)" :class="imageStyle(item)" />
         <div class="entry-content">
+          <img :src="item.icon" :alt="`${item.title} Icon`" class="icon" />
           <h3 class="title">
             {{ item.title }}
             <span v-if="item.pro" class="pro-badge">Pro</span>
           </h3>
           <p class="description">{{ item.synopsis }}</p>
-          <div class="meta">
+          <!-- <div class="meta">
             <div class="rating">Rating Stars</div>
-            <div class="likes">{{ item.downloads }} Likes</div>
-            <!-- <div v-if="item.category" class="category">{{ item.category.join(", ") }}</div> -->
-          </div>
+            <div class="likes">
+              <img src="./img/like.svg" :alt="`Like Icon`" class="like-icon" />
+              {{ item.downloads }} Likes
+            </div>
+          </div>-->
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 <script lang="ts">
@@ -40,12 +43,26 @@ export default {
     extensions: { type: Array, default: () => {} },
   },
   data() {
-    return {}
+    return {
+      loading: false,
+      active: 0,
+      timer: null,
+      animationInterval: 8000,
+    }
   },
   computed: {
     post(this: any) {
       return stored(this.postId) || {}
     },
+    currentExtension: function (this: any) {
+      return this.extensions[Math.abs(this.active) % this.extensions.length]
+    },
+    activeSlide(this: any) {
+      return this.extensions[this.active]
+    },
+  },
+  mounted() {
+    this.runTimer()
   },
   methods: {
     setting,
@@ -80,51 +97,76 @@ export default {
         }
       }
     },
+    nextSlide(this: any) {
+      if (this.active == this.extensions.length - 1) {
+        this.active = 0
+      } else {
+        this.active++
+      }
+
+      this.runTimer()
+    },
+    runTimer(this: any) {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => this.nextSlide(), this.animationInterval)
+    },
   },
 }
 </script>
 
 <style lang="less">
-.grid-plugin-container {
-  .section-header {
-    margin-bottom: 1rem;
-    .title {
-      font-size: 0.9rem;
-      text-transform: uppercase;
-      font-weight: 700;
-      line-height: 1.1;
-      letter-spacing: 0.5px;
-      color: var(--color-text-secondary);
-      @media (max-width: 900px) {
-        font-size: 1.7em;
-        line-height: 1.2;
+.slider-plugin-container {
+  margin-bottom: 6rem;
+
+  .slider-plugin {
+    position: relative;
+
+    .section-header {
+      margin-bottom: 1rem;
+      .title {
+        font-size: 0.9rem;
+        text-transform: uppercase;
+        font-weight: 700;
+        line-height: 1.1;
+        letter-spacing: 0.5px;
+        color: var(--color-text-secondary);
+        @media (max-width: 900px) {
+          font-size: 1.7em;
+          line-height: 1.2;
+        }
       }
     }
   }
 
-  .plugin-grid {
+  .featured-item-plugin {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
-    grid-gap: 2rem;
-
-    @media (max-width: 1200px) {
-      grid-template-columns: 1fr 1fr;
-    }
 
     @media (max-width: 900px) {
       grid-template-columns: 1fr;
     }
-  }
-  .grid-item-plugin {
-    display: block;
+    grid-gap: 2rem;
+    align-items: center;
     overflow: hidden;
     background: #fff;
     border-radius: 8px;
     box-shadow: var(--panel-shadow);
     cursor: pointer;
-    transition: 0.1s cubic-bezier(0.52, 0.01, 0.16, 1);
-    &:hover {
-      opacity: 0.9;
+    display: none;
+    opacity: 0;
+
+    &.active {
+      display: grid;
+      animation: opacity 0.4s forwards;
+    }
+
+    @keyframes opacity {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
     }
 
     .entry-media {
@@ -135,7 +177,8 @@ export default {
       max-width: 100%;
       position: relative;
       padding: 25%;
-      box-shadow: 0px 1px 0 rgba(50, 50, 93, 0.13);
+      min-height: 320px;
+      box-shadow: 1px 0 0 rgba(50, 50, 93, 0.13);
       &.icon {
         background-size: contain;
       }
@@ -151,8 +194,17 @@ export default {
     }
 
     .entry-content {
-      padding: 1.5rem;
-
+      padding: 2rem 1.5rem;
+      .icon {
+        display: block;
+        width: 64px;
+        height: 64px;
+        border-radius: 0.5rem;
+        box-shadow: 0 2.5px 5px -1px rgba(50, 50, 93, 0.25),
+          0 1.5px 3px -1.5px rgba(0, 0, 0, 0.3);
+        background-color: #fff;
+        margin-bottom: 1rem;
+      }
       .title {
         display: inline-flex;
         align-items: center;
@@ -177,6 +229,7 @@ export default {
         font-size: 1.2em;
         color: var(--color-text);
         opacity: 0.5;
+        // Unofficial line clamp works on all major browsers except IE
         display: -webkit-box;
         -webkit-line-clamp: 3;
         -webkit-box-orient: vertical;
@@ -188,6 +241,13 @@ export default {
         margin-top: 1rem;
         color: var(--color-text);
         opacity: 0.5;
+        .likes {
+          display: inline-flex;
+          align-items: center;
+          img {
+            margin-right: 0.3rem;
+          }
+        }
         .category {
           font-weight: var(--font-weight-bold, 700);
           text-transform: uppercase;
