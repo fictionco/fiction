b1c81663720387926a1e792e2077984c8a891c74
Andrew Powers <andrew@fiction.com>
2020-07-14 09:30:36 -0700

    fix: remove setup in GTM

--- @plugins/plugin-google-tag-manager/server.ts
+++ @plugins/plugin-google-tag-manager/server.ts
@@ -1,6 +1,4 @@
-import { addFilter, pushToFilter, setting } from "@factor/api"
-import { writeConfig, SetupCliConfig } from "@factor/cli/setup"
-import inquirer from "inquirer"
+import { pushToFilter, setting } from "@factor/api"
 
 export const setup = (): void => {
   const googleTagManagerId = setting("googleTagManager.googleTagManagerId")
@@ -12,43 +10,5 @@ export const setup = (): void => {
 
     return
   }
-
-  // CLI admin setup utility
-  addFilter({
-    key: "tagManagerSetup",
-    hook: "cli-add-setup",
-    callback: (_: SetupCliConfig[]) => {
-      const setupAdmins: SetupCliConfig = {
-        name: setupTitle,
-        value: "gtm",
-        callback: async (): Promise<void> => {
-          const questions = [
-            {
-              name: "googleTagManagerId",
-              message: "What's your Google Tag Manager container ID?",
-              type: "input",
-            },
-            {
-              name: "developmentMode",
-              type: "list",
-              message:
-                "Load the tag manager in your 'development' environment? (Defaults to production only. This can be changed later)",
-              choices: [
-                { name: "yes", value: true },
-                { name: "no", value: false },
-              ],
-            },
-          ]
-          const { googleTagManagerId, developmentMode } = await inquirer.prompt(questions)
-
-          await writeConfig("public", {
-            googleTagManager: { googleTagManagerId, developmentMode },
-          })
-        },
-      }
-
-      return [..._, setupAdmins]
-    },
-  })
 }
 setup()
