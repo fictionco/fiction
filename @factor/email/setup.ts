1da9b57d0c15fcc5646b51e88d9fba3362d4dfc9
Andrew Powers <andrew@fiction.com>
2020-07-14 07:39:33 -0700

    chore: remove setup

--- @factor/email/setup.ts
+++ @factor/email/setup.ts
@@ -1,6 +1,4 @@
-import { addFilter, pushToFilter } from "@factor/api"
-import { writeConfig, SetupCliConfig } from "@factor/cli/setup"
-import inquirer from "inquirer"
+import { pushToFilter } from "@factor/api"
 
 const { SMTP_USERNAME, SMTP_PASSWORD, SMTP_HOST } = process.env
 const key = "email"
@@ -18,47 +16,5 @@ export const setup = (): void => {
       },
     })
   }
-
-  // CLI admin setup utility
-  addFilter({
-    key,
-    hook: "cli-add-setup",
-    callback: (_: SetupCliConfig[]) => {
-      const setupItem = {
-        name: "Email SMTP - Setup",
-        value: "email",
-        callback: async (): Promise<void> => {
-          const questions = [
-            {
-              name: "SMTP_USERNAME",
-              message: "What's Your SMTP Service Username?",
-              type: "input",
-              default: process.env.SMTP_USERNAME,
-            },
-            {
-              name: "SMTP_PASSWORD",
-              message: "What's Your SMTP Service Password?",
-              type: "input",
-              default: process.env.SMTP_USERNAME,
-            },
-            {
-              name: "SMTP_HOST",
-              message: "What's Your SMTP Service Host?",
-              type: "input",
-              default: process.env.SMTP_HOST,
-            },
-          ]
-
-          const { SMTP_USERNAME, SMTP_PASSWORD, SMTP_HOST } = await inquirer.prompt(
-            questions
-          )
-
-          await writeConfig("private", { SMTP_USERNAME, SMTP_PASSWORD, SMTP_HOST })
-        },
-      }
-
-      return [..._, setupItem]
-    },
-  })
 }
 setup()
