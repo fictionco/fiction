8769ef1cb967469273e1e6726a3cae5ce79f60ac
Andrew Powers <andrew@fiction.com>
2020-07-22 14:07:04 -0700

    fix(darwin): ui

--- @apps/dashboard/src/onboard/v-onboard.vue
+++ @apps/dashboard/src/onboard/v-onboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <dashboard-wrapper class="onboard-view" :menu="menu" ppp="1">
+  <dashboard-wrapper class="onboard-view">
     <template #head>
       <div class="darwin-logo">
         <logo scheme="standard" />
@@ -7,31 +7,45 @@
     </template>
     <template #nav>
       <div class="onboard-menu">
-        <div class="title">Get Started</div>
+        <div class="title">Getting You Started</div>
+        <div class="menu-items">
+          <div v-for="(item, i) in menu" :key="i" class="item">
+            <div class="icon">
+              <div class="icon-wrap">
+                <factor-icon icon="fas fa-check" />
+              </div>
+            </div>
+            <div class="text">{{ item.name }}</div>
+          </div>
+        </div>
       </div>
     </template>
+    <dashboard-page title="Welcome to Darwin Analytics">
+      <dashboard-panel>test</dashboard-panel>
+    </dashboard-page>
   </dashboard-wrapper>
 </template>
 <script lang="ts">
-import { MenuGroup } from "@factor/ui/dashboard/types"
+import { factorIcon, dashboardPage, dashboardPanel } from "@factor/ui"
 import dashboardWrapper from "@factor/ui/dashboard/wrapper.vue"
 
 export default {
-  components: { dashboardWrapper, logo: () => import("../el/logo.vue") },
+  components: {
+    factorIcon,
+    dashboardWrapper,
+    dashboardPage,
+    dashboardPanel,
+    logo: () => import("../el/logo.vue"),
+  },
 
   data() {
     return {
       menu: [
-        {
-          group: "Get Started",
-          menu: [
-            { name: "Welcome", query: { step: 1 } },
-            { name: "Add Website", query: { step: 2 } },
-            { name: "Tracking Script", query: { step: 3 } },
-            { name: "Invite Team", query: { step: 4 } },
-          ],
-        },
-      ] as MenuGroup[],
+        { name: "Welcome", query: { step: 1 } },
+        { name: "Add Website", query: { step: 2 } },
+        { name: "Tracking Script", query: { step: 3 } },
+        { name: "Invite Team", query: { step: 4 } },
+      ],
     }
   },
 }
@@ -54,6 +68,32 @@ export default {
       text-transform: uppercase;
       font-size: 0.85em;
       letter-spacing: 0.02em;
+    }
+    .menu-items {
+      margin: 1rem 0;
+
+      .item {
+        padding: 0.5rem;
+        display: grid;
+        grid-template-columns: 2rem 1fr;
+        grid-gap: 0.5rem;
+        .icon-wrap {
+          background-color: var(--color-primary);
+          color: #fff;
+          border-radius: 2rem;
+          display: inline-block;
+          width: 1.5rem;
+          height: 1.5rem;
+          text-align: center;
+          line-height: 1.5rem;
+          .factor-icon {
+            font-size: 0.8em;
+          }
+        }
+        .text {
+          font-size: 1.1em;
+        }
+      }
     }
   }
 }
