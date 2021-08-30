c55433abd8af24b0b7c1c37812fbbe9912b46934
Andrew Powers <andrew@fiction.com>
2021-08-30 08:55:08 -0600

    fix: global dts

--- @core/types/index.ts
+++ @core/types/index.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./global.d.ts" />
 export * from "./attachment"
 export * from "./basics"
 export * from "./db"
