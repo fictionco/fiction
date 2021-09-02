156b18b500ef048ba59242387eb2ef830bbb730c
Andrew Powers <arpowers@gmail.com>
2021-09-02 15:46:32 -0700

    chore: tweaks for workflow

--- @core/render/tailwind.config.js
+++ @core/render/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   mode: "jit",
   purge: {
-    content: ["./src/**/*.{vue,js,ts,jsx,tsx,html}"],
+    content: [
+      "./src/**/*.{vue,js,ts,jsx,tsx,html}",
+      ...require("@factor/server-utils/tailwind").paths,
+    ],
   },
 }
