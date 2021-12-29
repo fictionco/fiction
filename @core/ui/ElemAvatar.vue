ea7962ec1a4c47f9914f5a49a398a3957b8141e0
Andrew Powers <andrew@fiction.com>
2021-12-29 15:17:22 -0700

    fix: dep

--- @core/ui/ElemAvatar.vue
+++ @core/ui/ElemAvatar.vue
@@ -5,38 +5,32 @@
     :data-test="src"
   />
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import { stored } from "@factor/api"
 import { PublicUser } from "@factor/types"
 import gravatar from "gravatar"
 import { computed } from "vue"
-export default {
-  components: {},
-  props: {
-    userId: { type: String, default: "" },
-    url: { type: String, default: "" },
-    email: { type: String, default: "" },
-  },
-  setup(props) {
-    const user = computed<PublicUser | undefined>(() => {
-      if (!props.userId) return undefined
-      return stored(props.userId) ?? undefined
-    })
 
-    const src = computed<string>(() => {
-      if (props.url) {
-        return props.url
-      } else if (user.value && user.value.avatar) {
-        return user.value.avatar
-      } else if (user.value || props.email) {
-        const email = user.value ? user.value.email : props.email
-        return gravatar.url(email, { s: "200", d: "retro" }) || ""
-      } else {
-        return ""
-      }
-    })
+const props = defineProps({
+  userId: { type: String, default: "" },
+  url: { type: String, default: "" },
+  email: { type: String, default: "" },
+})
+const user = computed<PublicUser | undefined>(() => {
+  if (!props.userId) return undefined
+  return stored(props.userId) ?? undefined
+})
 
-    return { src }
-  },
-}
+const src = computed<string>(() => {
+  if (props.url) {
+    return props.url
+  } else if (user.value && user.value.avatar) {
+    return user.value.avatar
+  } else if (user.value || props.email) {
+    const email = user.value ? user.value.email : props.email
+    return gravatar.url(email, { s: "200", d: "retro" }) || ""
+  } else {
+    return ""
+  }
+})
 </script>
