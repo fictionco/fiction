9953b9b9b0b0bfff84579b265b00838078fe7c73
Andrew Powers <arpowers@gmail.com>
2022-02-02 17:30:10 -0800

    fix: tailwind

--- @core/render/tailwind.config.ts
+++ @core/render/tailwind.config.ts
@@ -1,8 +1,16 @@
+import path from "path"
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url)
+
+const modulePath = (mod: string): string =>
+  path.dirname(require.resolve(`${mod}/package.json`))
+
 export default {
   mode: "jit",
   content: [
     "./src/**/*.{vue,js,ts,jsx,tsx,html}",
-    "@factor/ui/*.vue",
-    "@factor/plugin-notify/*.vue",
+    `${modulePath("@factor/ui")}/*.vue`,
+    `${modulePath("@factor/plugin-notify")}/*.vue`,
   ],
 } as Record<string, any>
