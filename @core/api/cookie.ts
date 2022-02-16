5004b9088bec7441c017237a1b9a1aeef72223ba
Andrew Powers <andrew@fiction.com>
2022-02-15 19:45:20 -0700

    fix: f

--- @core/api/cookie.ts
+++ @core/api/cookie.ts
@@ -23,3 +23,26 @@ export const removeCookie = (
 ): void => {
   return Cookies.remove(name, attributes)
 }
+
+/**
+ * Gets the domain name without any sub domains.
+ * Needed for cookies, this is not made available any easier way.
+ * It works by setting cookies until they can't be set
+ * https://developers.livechat.com/updates/setting-cookies-to-subdomains-in-javascript
+ * http://jsfiddle.net/zEwsP/4/
+ */
+export const getTopDomain = (): string => {
+  let i = 0
+  let domain = document.domain
+  const p = domain.split(".")
+  const s = `_gd${Date.now()}`
+  while (i < p.length - 1 && !document.cookie.includes(s + "=" + s)) {
+    domain = p.slice(-1 - ++i).join(".")
+    // eslint-disable-next-line unicorn/no-document-cookie
+    document.cookie = s + "=" + s + ";domain=" + domain + ";"
+  }
+  // eslint-disable-next-line unicorn/no-document-cookie
+  document.cookie =
+    s + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=" + domain + ";"
+  return domain
+}
