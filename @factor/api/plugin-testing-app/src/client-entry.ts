7904115e311a403b663258905bf75147479cf15f
Andrew Powers <arpowers@gmail.com>
2022-07-27 09:41:17 +0000

    chore: pre-release

--- @factor/api/plugin-testing-app/src/client-entry.ts
+++ @factor/api/plugin-testing-app/src/client-entry.ts
@@ -1,5 +1,11 @@
 import { initApp } from "."
 
+// prevent 'process' not defined errors in browser
+if (typeof window !== "undefined" && typeof window.process == "undefined") {
+  // @ts-ignore (avoid confusion with node process.env)
+  window.process = { env: {} }
+}
+
 const { app, router } = initApp({ env: "client" })
 
 // wait until router is ready before mounting to ensure hydration match
