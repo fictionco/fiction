b2e0b6720eecf52ce0cd4803fd596b7a62343e45
Andrew Powers <arpowers@gmail.com>
2022-07-28 09:55:13 -0700

    fix: test

--- @factor/api/plugin-testing-app/test/spa.test.ts
+++ @factor/api/plugin-testing-app/test/spa.test.ts
@@ -12,7 +12,6 @@ describe("spa test app server", () => {
     const testingApp = new FactorTestingApp({
       port,
       head: `<meta name="author" content="John Doe">`,
-      mode: "development",
     })
     server = await testingApp.createApp()
     browser = await chromium.launch()
@@ -26,17 +25,7 @@ describe("spa test app server", () => {
     await page?.goto(`http://localhost:${port}`)
     const ht = await page?.content()
     expect(server).toBeDefined()
-    expect(ht).toMatchInlineSnapshot(`
-      "<!DOCTYPE html><html lang=\\"en\\"><head>
-      <meta charset=\\"utf-8\\">
-      <title>Error</title>
-      </head>
-      <body>
-      <pre>Error: Failed to parse source for import analysis because the content contains invalid JS syntax. Install @vitejs/plugin-vue to handle .vue files.<br> &nbsp; &nbsp;at formatError (file:///Users/arpowers/Projects/factor/node_modules/.pnpm/vite@3.0.3_less@4.1.3/node_modules/vite/dist/node/chunks/dep-c6273c7a.js:35035:46)<br> &nbsp; &nbsp;at TransformContext.error (file:///Users/arpowers/Projects/factor/node_modules/.pnpm/vite@3.0.3_less@4.1.3/node_modules/vite/dist/node/chunks/dep-c6273c7a.js:35031:19)<br> &nbsp; &nbsp;at TransformContext.transform (file:///Users/arpowers/Projects/factor/node_modules/.pnpm/vite@3.0.3_less@4.1.3/node_modules/vite/dist/node/chunks/dep-c6273c7a.js:40107:22)<br> &nbsp; &nbsp;at async Object.transform (file:///Users/arpowers/Projects/factor/node_modules/.pnpm/vite@3.0.3_less@4.1.3/node_modules/vite/dist/node/chunks/dep-c6273c7a.js:35284:30)<br> &nbsp; &nbsp;at async loadAndTransform (file:///Users/arpowers/Projects/factor/node_modules/.pnpm/vite@3.0.3_less@4.1.3/node_modules/vite/dist/node/chunks/dep-c6273c7a.js:39803:29)</pre>
 
-
-      </body></html>"
-    `)
     expect(ht).toContain("John Doe")
     expect(ht?.length).toBeGreaterThan(100)
   })
