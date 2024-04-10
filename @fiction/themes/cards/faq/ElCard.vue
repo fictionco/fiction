65fa414701e45d22d311bd16da21b088e1253ad7
Andrew Powers <arpowers@gmail.com>
2024-04-10 11:57:19 -0700

    fix: faq -d

--- @fiction/themes/cards/faq/ElCard.vue
+++ @fiction/themes/cards/faq/ElCard.vue
@@ -30,17 +30,17 @@ const uc = vue.computed(() => {
         <div class="divider h-[1px] dark:bg-theme-800 bg-theme-200 my-6" />
         <div
 
-          class="py-4 flex justify-between gap-6 items-center "
+          class="py-4 flex flex-col-reverse lg:flex-row justify-between gap-6 lg:items-center "
         >
-          <div class="space-y-2">
-            <div class="text-2xl lg:text-4xl x-font-title font-semibold">
+          <div class="space-y-2 max-w-screen-md">
+            <div class="text-2xl lg:text-4xl x-font-title font-semibold text-balance ">
               {{ item.name }}
             </div>
-            <div class="text-lg lg:text-xl">
+            <div class="text-lg lg:text-xl text-balance leading-relaxed lg:leading-relaxed text-theme-600 dark:text-theme-100">
               {{ item.desc }}
             </div>
           </div>
-          <div class="text-5xl lg:text-8xl font-sans font-light">
+          <div class="text-5xl lg:text-8xl font-sans font-light antialiased text-theme-500">
             {{ String(i + 1).padStart(2, '0') }}
           </div>
         </div>
--- @fiction/themes/fiction/pricing/index.ts
+++ @fiction/themes/fiction/pricing/index.ts
@@ -83,12 +83,11 @@ const valueCard = createCard({
   templates,
   templateId: 'faq',
   userConfig: {
-    heading: `Values`,
+    heading: `Frequently Asked Questions`,
     items: [
-      { name: 'Focused', desc: `Create big value for a small group of people. Don't try and be everything to everyone.` },
-      { name: `Karma`, desc: `Focus on making a contribution, the rest takes care of itself.` },
-      { name: `Crafted`, desc: `Take the time to do things extremely well. It's better to do nothing, than release something below our standards.` },
-      { name: `Minimal`, desc: `Simplicity is the ultimate form of elegance. Do what's needed and nothing more.` },
+      { name: 'Can I Cancel My Subscription At Any Time?', desc: `Of course! If you decide that Fiction  isn't the right fit for your business, you can easily cancel your account from your dashboard at any time.` },
+      { name: `Does Fiction Take A Cut Of My Revenue?`, desc: `No! When you make a sale with Fiction, we don't take a percentage cut of your revenue from that sale (unlike most creator platforms). If you use Stripe or Paypal to collect payments, you will still pay their merchant processing fees (for example, Stripe's merchant processing fee is 2.9% + 30 cents per transaction).` },
+      { name: `What should my personal marketing platform include?`, desc: `A successful platform should feature a consistent stream of content that your audience finds valuable. This could include articles, videos, audios, courses, live webinars, downloadable resources, perks (like event tickets or physical merchandise), and/or a community section or forum.` },
 
     ],
   },
