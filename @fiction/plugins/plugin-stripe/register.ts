7ee2999a06d8a15d74ac4b09279ef3657fe47e72
Andrew Powers <arpowers@gmail.com>
2024-05-31 18:29:24 -0700

    fix: dep 6 -d

--- @fiction/plugins/plugin-stripe/register.ts
+++ @fiction/plugins/plugin-stripe/register.ts
@@ -1,9 +1,11 @@
 import { EnvVar, vars } from '@fiction/core/plugin-env'
 
 vars.register(() => [
-  new EnvVar({ name: 'STRIPE_PUBLIC_KEY_TEST', isPublic: true }),
-  new EnvVar({ name: 'STRIPE_SECRET_KEY_TEST', isPublic: false }),
-  new EnvVar({ name: 'STRIPE_PUBLIC_KEY_PROD', isPublic: true, verify: ({ fictionEnv, value }) => { return !(fictionEnv.isProd.value && !value && fictionEnv.isApp.value) } }),
+  new EnvVar({ name: 'STRIPE_PUBLIC_KEY_TEST', isPublic: true, isOptional: true }),
+  new EnvVar({ name: 'STRIPE_SECRET_KEY_TEST', isPublic: false, isOptional: true }),
+  new EnvVar({ name: 'STRIPE_PUBLIC_KEY_PROD', isPublic: true, verify: ({ fictionEnv, value }) => {
+    return !(fictionEnv.isProd.value && !value && fictionEnv.isApp.value)
+  } }),
   new EnvVar({ name: 'STRIPE_SECRET_KEY_PROD', verify: ({ fictionEnv, value }) => {
     return !(fictionEnv.isProd.value && !value && !fictionEnv.isApp.value && !fictionEnv.isCi)
   }, isPublic: false }),
