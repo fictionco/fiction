702e09a7e8b5891f4e298c5e7508d2d0050aa9ac
Andrew Powers <arpowers@gmail.com>
2024-06-26 11:18:32 -0700

    chore: deps -d

--- @fiction/www/.fiction/config.ts
+++ @fiction/www/.fiction/config.ts
@@ -66,6 +66,7 @@ export interface CompiledServiceConfig {
     | 'ManagePage'
     | 'ManagePost'
     | 'ManagePostIndex'
+    | 'ManageSend'
     | 'ManageSite'
     | 'ManageSubscription'
     | 'ManageTaxonomy'
