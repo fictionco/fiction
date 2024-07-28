9456945cc5f9c4dd1d55d1a17b8f01eac2c7a69f
Andrew Powers <arpowers@gmail.com>
2024-07-28 14:09:48 -0700

    fix: test -d

--- @fiction/core/utils/test/fonts.unit.test.ts
+++ @fiction/core/utils/test/fonts.unit.test.ts
@@ -29,7 +29,7 @@ describe('variantToGoogleFontsFormat', () => {
 describe('createGoogleFontsLink', () => {
   it('should return correct URL for valid font keys', () => {
     const fontLink = createGoogleFontsLink({ fontKeys: ['Roboto', 'Open Sans'] })
-    const expectedLink = 'https://fonts.googleapis.com/css2?family=Roboto&family=Open+Sans&display=swap'
+    const expectedLink = 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
     expect(fontLink).toBe(expectedLink)
   })
 
